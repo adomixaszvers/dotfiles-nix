73c8d9265d71a022b6cd03255e175e4a1f68fb8b
Adomas Jatužis <adomixaszvers@gmail.com>
2018-09-16 22:54:04 +0300

    Add .Xresources

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -21,6 +21,53 @@
     enable = true;
     theme = "Monokai";
   };
+  xresources.extraConfig = ''
+    URxvt*font: xft:Source Code Pro:size=10 
+    URxvt.perl-ext-common: default,matcher,selection-to-clipboard,resize-font
+    URxvt.url-launcher: /usr/bin/xdg-open
+    URxvt.matcher.button: 1
+    URxvt.letterSpace: -1
+    URxvt.iso14755: false
+    URxvt.iso14755_52: false
+    URxvt.scrollBar:	false
+
+    ! special
+    *.foreground:   #93a1a1
+    *.background:   #002b36
+    *.cursorColor:  #93a1a1
+
+    ! black
+    *.color0:       #002b36
+    *.color8:       #657b83
+
+    ! red
+    *.color1:       #dc322f
+    *.color9:       #dc322f
+
+    ! green
+    *.color2:       #859900
+    *.color10:      #859900
+
+    ! yellow
+    *.color3:       #b58900
+    *.color11:      #b58900
+
+    ! blue
+    *.color4:       #268bd2
+    *.color12:      #268bd2
+
+    ! magenta
+    *.color5:       #6c71c4
+    *.color13:      #6c71c4
+
+    ! cyan
+    *.color6:       #2aa198
+    *.color14:      #2aa198
+
+    ! white
+    *.color7:       #93a1a1
+    *.color15:      #fdf6e3
+  '';
   xsession.enable = true;
   xsession.windowManager.i3 = import ./i3.nix { inherit pkgs; };
 }
