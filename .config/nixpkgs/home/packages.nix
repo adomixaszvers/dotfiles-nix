8db846a11c303c05b36aa1a2ff5fe06115f1188b
Adomas Jatužis <adomixaszvers@gmail.com>
2018-09-23 15:21:35 +0300

    Remove let from packages.nix

--- .config/nixpkgs/home/packages.nix
+++ .config/nixpkgs/home/packages.nix
@@ -1,53 +1,51 @@
 pkgs:
 with pkgs;
-let
-    in
-    {
-      common = [
-        arandr
-        mine.bumblebee-status
-        file
-        git
-        gnome3.adwaita-icon-theme
-        gnome3.gnome-screenshot
-        google-chrome
-        i3lock
-        htop
-        keepass
-        meld
-        ncdu
-        notify-osd
-        evince
-        ranger
-        rxvt_unicode-with-plugins
-        thefuck
-        tree
-        vcsh
-        xfce.gvfs
-        xfce.thunar-bare
-        xsel
-      ];
-      home = [
-        discord
-        unstablePkgs.exercism
-        ghc
-        stack
-        torbrowser
-        vim
-        mine.vimgolf
-        vlc
-        qbittorrent
-        unstablePkgs.typora
-      ];
-      work = [
-        mine.consul
-        jdk8
-        jetbrains.idea-ultimate
-        libreoffice-fresh
-        maven
-        nodejs
-        yarn
-        unstablePkgs.smartgithg
-        unstablePkgs.skype
-      ];
-    }
+{
+  common = [
+    arandr
+    mine.bumblebee-status
+    file
+    git
+    gnome3.adwaita-icon-theme
+    gnome3.gnome-screenshot
+    google-chrome
+    i3lock
+    htop
+    keepass
+    meld
+    ncdu
+    notify-osd
+    evince
+    ranger
+    rxvt_unicode-with-plugins
+    thefuck
+    tree
+    vcsh
+    xfce.gvfs
+    xfce.thunar-bare
+    xsel
+  ];
+  home = [
+    discord
+    unstablePkgs.exercism
+    ghc
+    stack
+    torbrowser
+    vim
+    mine.vimgolf
+    vlc
+    qbittorrent
+    unstablePkgs.typora
+  ];
+  work = [
+    mine.consul
+    jdk8
+    jetbrains.idea-ultimate
+    libreoffice-fresh
+    maven
+    nodejs
+    yarn
+    unstablePkgs.smartgithg
+    unstablePkgs.skype
+  ];
+}
