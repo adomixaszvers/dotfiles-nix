c79a5ca28decba3080ac98e4ce912334b4513dc8
Adomas Jatužis <adomixaszvers@gmail.com>
2018-10-19 20:45:45 +0300

    Change home manager path

--- .config/nixpkgs/home/default.nix
+++ .config/nixpkgs/home/default.nix
@@ -122,7 +122,7 @@
     enableZshIntegration = true;
   };
   programs.home-manager.enable = true;
-  programs.home-manager.path = https://github.com/rycee/home-manager/archive/release-18.03.tar.gz;
+  programs.home-manager.path = https://github.com/rycee/home-manager/archive/release-18.09.tar.gz;
   programs.feh.enable = true;
   programs.rofi = {
     enable = true;
