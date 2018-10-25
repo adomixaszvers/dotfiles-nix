3372165fd175c49881f1a8a8997108cca7a697bc
Adomas Jatužis <adomixaszvers@gmail.com>
2018-10-25 23:11:07 +0300

    Change Neovim configuration

--- .config/nixpkgs/home/neovim.nix
+++ .config/nixpkgs/home/neovim.nix
@@ -9,31 +9,31 @@
           set path+=**
           set list
           set inccommand=nosplit
+          colorscheme google
 
           let g:neomake_open_list = 2
+          call neomake#configure#automake('nrwi', 500)
+
+          let g:hardtime_default_on = 1
           let g:hardtime_showmsg = 1
           let g:hardtime_allow_different_key = 1
-
-          colorscheme google
-          " packadd neomake
-          call neomake#configure#automake('nrwi', 500)
-          let g:hardtime_default_on = 1
-          let g:airline_powerline_fonts = 1
       '';
       vam.knownPlugins = pkgs.vimPlugins;
       vam.pluginDictionaries = [
+        { name = "commentary"; }
         { name = "fugitive"; }
-        { name = "surround"; }
-        { name = "vinegar"; }
-        { name = "repeat"; }
-        { name = "neomake"; }
-        { name = "rust-vim"; }
-        { name = "syntastic"; }
-        { name = "vim-colorschemes"; }
-        { name = "vim-addon-nix"; }
-        { name = "vim-nix"; }
-        { name = "vim-hardtime"; }
         { name = "lightline-vim"; }
+        { name = "neomake"; }
+        { name = "repeat"; }
+        { name = "rust-vim"; }
+        { name = "surround"; }
+        { name = "syntastic"; }
+        { name = "vim-addon-nix"; }
+        { name = "vim-colorschemes"; }
+        { name = "vim-easymotion"; }
+        { name = "vim-hardtime"; }
+        { name = "vim-nix"; }
+        { name = "vinegar"; }
       ];
     };
   };
