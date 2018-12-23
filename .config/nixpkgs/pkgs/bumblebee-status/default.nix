b8367db10b2d60c27dc4b2d668880807562ed2b0
Adomas Jatužis <adomixaszvers@gmail.com>
2018-12-23 10:57:42 +0200

    Change bumblebee-status

--- .config/nixpkgs/pkgs/bumblebee-status/default.nix
+++ .config/nixpkgs/pkgs/bumblebee-status/default.nix
@@ -14,10 +14,13 @@ in stdenv.mkDerivation {
   ))];
   unpackPhase = ":";
   installPhase = ''
-    install -d $out/opt/bumblebee-status
-    cp -rp $src/* $out/opt/bumblebee-status
+    install -d $out/share/bumblebee-status
+    cp -rp $src/* $out/share/bumblebee-status
     install -d $out/bin
-    ln -s $out/opt/bumblebee-status/bumblebee-status $out/bin/bumblebee-status
+    ln -s $out/share/bumblebee-status/bumblebee-status $out/bin/bumblebee-status
   '';
-  meta = {};
+  meta = {
+    description = "bumblebee-status is a modular, theme-able status line generator for the i3 window manager.";
+    homepage = https://github.com/tobi-wan-kenobi/bumblebee-status;
+  };
 }
