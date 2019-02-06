35e655635f711900ae3cd3d6f8de463047894c50
Adomas Jatužis <adomixaszvers@gmail.com>
2019-02-06 07:28:54 +0200

    Refactor install script

--- .config/nixpkgs/install-home-manager.sh
+++ .config/nixpkgs/install-home-manager.sh
@@ -1,5 +1,5 @@
-#! /usr/bin/env bash
+#! /usr/bin/env sh
 HM_PATH=https://github.com/rycee/home-manager/archive/release-18.09.tar.gz
 nix-channel --add $HM_PATH home-manager && \
     nix-channel --update home-manager && \
-    nix-shell -I home-manager=$HOME/.nix-defexpr/channels/home-manager '<home-manager>' -A install
+    nix-shell -I home-manager="$HOME/.nix-defexpr/channels/home-manager" '<home-manager>' -A install
