1f1fd42ba3dbec1aa8ec91b8232cb8c3a7937de3
Adomas Jatužis <adomixaszvers@gmail.com>
2019-02-21 23:34:44 +0200

    Emacs use-package

--- home/dotfiles/emacs.d/init.el
+++ home/dotfiles/emacs.d/init.el
@@ -1,14 +1,28 @@
+(require 'package)
+
+;; optional. makes unpure packages archives unavailable
+(setq package-archives nil)
+
+(setq package-enable-at-startup nil)
+(package-initialize)
 (setq-default display-line-numbers-type 'relative
               display-line-numbers-current-absolute t
               display-line-numbers-width 4
               display-line-numbers-widen t)
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.emacs-saves/"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+(set-frame-font "FuraMono Nerd Font-9")
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (add-hook 'text-mode-hook #'display-line-numbers-mode)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
 (setq display-line-numbers 'relative)
-(eval-when-compile
-    (package-initialize)
-    (require 'use-package))
+(require 'use-package)
 (use-package evil-leader
     :ensure t
     :after evil
@@ -25,7 +39,7 @@
     :ensure t
     :init
     (setq evil-want-keybinding nil)
-    (setq evil-want-integration nil) ;; This is optional since it's already set to t by default.
+    (setq evil-want-integration nil)
     (setq evil-want-C-u-scroll t)
     :config
     (evil-mode 1))
