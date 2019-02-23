54e7f9624d41095e5eae09a5aa971edae31e7d74
Adomas Jatužis <adomixaszvers@gmail.com>
2019-02-23 12:05:05 +0200

    Remote unnecessary packages

--- home/hosts/home.nix
+++ home/hosts/home.nix
@@ -3,23 +3,15 @@
   imports = [ ../emacs.nix ];
   home.packages = with pkgs;
   [
-    # sageWithDoc
     borgbackup
     discord
-    elmPackages.elm
     exercism
     firefox
-    jdk
-    jetbrains.idea-ultimate
-    mine.ghc
+    gtypist
     mine.steam
-    (with import <hie-nix> {}; hies)
     mine.vimgolf
     qbittorrent
-    sbt
-    stack
     torbrowser
-    # typora
     vim
     vlc
   ];
