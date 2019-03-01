74573aa8980af2d28ef61fbfc7d770a39daa6305
Adomas Jatužis <adomixaszvers@gmail.com>
2019-03-01 12:26:51 +0200

    Check if bspwm processes are running

--- home/bspwm.nix
+++ home/bspwm.nix
@@ -4,11 +4,11 @@
   xdg.configFile."bspwm/bspwmrc" = {
     source = ./dotfiles/bspwmrc;
     executable = true;
-    onChange = "bspc wm -r";
+    onChange = "pidof bspwm 1>/dev/null && bspc wm -r";
   };
   xdg.configFile."sxhkd/sxhkdrc" = {
     source = ./dotfiles/sxhkdrc;
-    onChange = "pkill -USR1 -x sxhkd";
+    onChange = "pidof sxhkd 1>/dev/null && pkill -USR1 -x sxhkd";
   };
   xsession.windowManager.command = "bspwm";
 }
