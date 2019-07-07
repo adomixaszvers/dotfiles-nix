6dacd495e46bca8db71e20177ceb7f84975bc6cb
Adomas Jatužis <adomixaszvers@gmail.com>
2019-07-07 10:53:13 +0300

    Set emacs custom file

--- home/dotfiles/emacs.d/init.el
+++ home/dotfiles/emacs.d/init.el
@@ -20,3 +20,7 @@
 ;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
 (when (file-readable-p "~/.emacs.d/config.el")
   (load-file (expand-file-name "~/.emacs.d/config.el")))
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-readable-p custom-file)
+  (load custom-file))
