ca7a050f04330d477392701b13d4a1eb945aef5d
Adomas Jatužis <adomixaszvers@gmail.com>
2019-07-13 23:05:57 +0300

    Disable HardTime in Neovim

--- home/neovim.nix
+++ home/neovim.nix
@@ -57,7 +57,7 @@ in {
 
         set noshowmode
 
-        let g:hardtime_default_on = 1
+        let g:hardtime_default_on = 0
         let g:hardtime_showmsg = 1
         let g:hardtime_allow_different_key = 1
 
