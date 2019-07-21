68eedad3f75bf0af137eb8427f15d5f66f890582
Adomas Jatužis <adomixaszvers@gmail.com>
2019-07-21 22:08:15 +0300

    Change awesomewm layouts order

--- home/dotfiles/awesome/rc.lua
+++ home/dotfiles/awesome/rc.lua
@@ -63,6 +63,7 @@ modkey = "Mod4"
 awful.layout.layouts = {
     awful.layout.suit.spiral,
     awful.layout.suit.tile,
+    awful.layout.suit.max,
     awful.layout.suit.floating,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -70,7 +71,6 @@ awful.layout.layouts = {
     awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
     awful.layout.suit.corner.nw,
