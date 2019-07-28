f98155b576a0341b60b9f722d6f718fb766261b8
Adomas Jatužis <adomixaszvers@gmail.com>
2019-07-28 21:40:47 +0300

    Update XMonad config

--- home/dotfiles/xmonad.hs
+++ home/dotfiles/xmonad.hs
@@ -1,10 +1,16 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 import           System.Exit                    ( exitSuccess )
 import           XMonad
-import           XMonad.Hooks.EwmhDesktops      ( ewmh )
+import           XMonad.Hooks.EwmhDesktops      ( ewmh
+                                                , fullscreenEventHook
+                                                )
 import           XMonad.Hooks.ManageDocks       ( avoidStruts
                                                 , docks
                                                 , manageDocks
+                                                , ToggleStruts(ToggleStruts)
+                                                )
+import           XMonad.Hooks.ManageHelpers     ( composeOne
+                                                , (-?>)
                                                 )
 import           XMonad.Layout.NoBorders        ( smartBorders )
 import           XMonad.Layout.Spacing          ( Border(..)
@@ -32,8 +38,9 @@ myConfig =
           , focusedBorderColor = "#8BE9FD"
           , modMask            = mod4Mask
           , borderWidth        = 2
+          , handleEventHook    = fullscreenEventHook <+> handleEventHook def
           , layoutHook         = myMainLayout
-          , manageHook         = myManageHook <+> manageDocks <+> manageHook def
+          , manageHook         = manageDocks <+> myManageHook <+> manageHook def
           , startupHook        = myStartupHook
           }
     `removeKeysP` ["M-p", "M-S-p"]
@@ -55,14 +62,14 @@ myMainLayout = smartBorders . avoidStruts $ tiled ||| Mirror tiled ||| Full
   outer = 3
   inner = 5
 
-myManageHook = composeAll
-  [ className =? "Google-chrome" <||> className =? "Firefox" --> doShift "1"
-  , className =? "jetbrains-idea" --> doShift "3"
-  , className =? "rambox" --> doShift "4"
-  , className =? "Steam" <||> className =? "SmartGit" --> doShift "5"
-  , className =? "libreoffice" --> doShift "6"
-  , className =? "Emacs" --> doShift "7"
-  , className =? "google play music desktop player" --> doShift "9"
+myManageHook = composeOne
+  [ className =? "Google-chrome" <||> className =? "Firefox" -?> doShift "1"
+  , className =? "jetbrains-idea" -?> doShift "3"
+  , className =? "rambox" -?> doShift "4"
+  , className =? "Steam" <||> className =? "SmartGit" -?> doShift "5"
+  , className =? "libreoffice" -?> doShift "6"
+  , className =? "Emacs" -?> doShift "7"
+  , className =? "google play music desktop player" -?> doShift "9"
   ]
 
 myAdditionalKeys c =
@@ -77,27 +84,27 @@ myAdditionalKeys c =
           , 0x3fe -- ū
           , 0xafe -- „
           ]
-  in  (subtitle "Custom Keys" :)
-        $  mkNamedKeymap
-             c
-             [ ( "M-d"
-               , addName "Open DRun menu"
-                 $ spawn "rofi -show combi -combi-modi window,drun"
-               )
-             , ( "M-S-d"
-               , addName "Open Run menu" $ spawn "rofi -show run -sidebar-mode"
-               )
-             , ("M-S-<Return>", addName "Swap master" $ windows W.swapMaster)
-             , ("M-<Return>"  , addName "Spawn terminal" $ spawn myTerminal)
-             , ("M-S-q"       , addName "Kill client" kill)
-             , ("M-S-c"       , addName "Exit XMonad" $ io exitSuccess)
-             ]
-        ++ [ ( (m .|. myModMask, key)
-             , addName (name ++ " " ++ i) $ windows $ f i
-             )
-           | (i, key) <- zip (XMonad.workspaces c) ltKeys
-           , (f, name, m) <-
-             [ (W.greedyView, "Open workspace"   , 0)
-             , (W.shift     , "Move to workspace", shiftMask)
-             ]
-           ]
+  in
+    (subtitle "Custom Keys" :)
+    $  mkNamedKeymap
+         c
+         [ ("M-b", addName "Toggle struts" $ sendMessage ToggleStruts)
+         , ( "M-d"
+           , addName "Open DRun menu"
+             $ spawn "rofi -show combi -combi-modi window,drun"
+           )
+         , ( "M-S-d"
+           , addName "Open Run menu" $ spawn "rofi -show run -sidebar-mode"
+           )
+         , ("M-S-<Return>", addName "Swap master" $ windows W.swapMaster)
+         , ("M-<Return>"  , addName "Spawn terminal" $ spawn myTerminal)
+         , ("M-S-q"       , addName "Kill client" kill)
+         , ("M-S-c"       , addName "Exit XMonad" $ io exitSuccess)
+         ]
+    ++ [ ((m .|. myModMask, key), addName (name ++ " " ++ i) $ windows $ f i)
+       | (i, key) <- zip (XMonad.workspaces c) ltKeys
+       , (f, name, m) <-
+         [ (W.greedyView, "Open workspace"   , 0)
+         , (W.shift     , "Move to workspace", shiftMask)
+         ]
+       ]
