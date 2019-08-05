65a59b84483c1148bfedd994e793b4eecee973f5
Adomas Jatužis <adomixaszvers@gmail.com>
2019-08-05 21:06:38 +0300

    XMonad: use 13.30 resolver and dynamic linking

--- home/xmonad.nix
+++ home/xmonad.nix
@@ -5,7 +5,7 @@
   };
   home.packages = with pkgs; [
     (with import <nixos-unstable> { }; haskellPackages.brittany)
-    (with import <all-hies> {  }; versions.ghc864)
+    (with import <all-hies> {  }; versions.ghc865)
     gnome3.zenity
     haskellPackages.hlint
     mine.maimpick
