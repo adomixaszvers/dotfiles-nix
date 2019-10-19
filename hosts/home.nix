6df651eb04d31ccd7bc87ec72c28daf59e605a10
Adomas Jatužis <adomixaszvers@gmail.com>
2019-10-19 11:41:58 +0300

    reformat using nixfmt

--- hosts/home.nix
+++ hosts/home.nix
@@ -1,19 +1,21 @@
 { pkgs, config, ... }: {
-  home.packages = with pkgs; let unstable = channels.nixos-unstable; in [
-    borgbackup
-    calibre
-    discord
-    exercism
-    firefox
-    gtypist
-    guile
-    klavaro
-    minecraft
-    (mine.steam.override { config.steam.primus = true; })
-    mine.vimgolf
-    qbittorrent
-    torbrowser
-    unstable.jetbrains.idea-ultimate
-    vim
-  ];
+  home.packages = with pkgs;
+    let unstable = channels.nixos-unstable;
+    in [
+      borgbackup
+      calibre
+      discord
+      exercism
+      firefox
+      gtypist
+      guile
+      klavaro
+      minecraft
+      (mine.steam.override { config.steam.primus = true; })
+      mine.vimgolf
+      qbittorrent
+      torbrowser
+      unstable.jetbrains.idea-ultimate
+      vim
+    ];
 }
