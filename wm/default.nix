7bfddae7ba7a371e907086cd6aed94e2ff2fcb00
Adomas Jatužis <adomixaszvers@gmail.com>
2019-11-11 11:44:02 +0200

    Enable udiskie

--- wm/default.nix
+++ wm/default.nix
@@ -3,6 +3,7 @@
 {
   imports = [ ./xmonad ./polybar.nix ./dunst.nix ./compton.nix ];
   services.network-manager-applet.enable = config.xsession.enable;
+  services.udiskie.enable = true;
   xsession.enable = lib.mkDefault true;
   xsession.initExtra = ''
     xset s off -dpms
