36f8236111248c484c40904aef3f1dd1b91b8812
Adomas Jatužis <adomixaszvers@gmail.com>
2019-12-26 11:37:04 +0200

    add 4ban colors
    Former-commit-id: 48654e0d757b26224aae30570bd7c461880c66f8

--- gui/colors.nix
+++ gui/colors.nix
@@ -8,6 +8,37 @@ let
       g = f 3;
       b = f 5;
     in "rgba(${r}, ${g}, ${b}, 0.95)";
+  _4ban = {
+    foreground = "#f2efed";
+    background = "#282828";
+    backgroundTRGBA = toTrgba "#282828";
+    backgroundTransparent = "[95]#282828";
+    cursorColor = "#c8ccd4";
+
+    black = "#32302f";
+    blackb = "#928374";
+
+    red = "#d64d4d";
+    redb = "#fb4934";
+
+    green = "#00b159";
+    greenb = "#00b159";
+
+    yellow = "#f18e38";
+    yellowb = "#cc9c00";
+
+    blue = "#428bca";
+    blueb = "#428bca";
+
+    magenta = "#B85C8A";
+    magentab = "#B85C8A";
+
+    cyan = "#669c9c";
+    cyanb = "#669c9c";
+
+    white = "#a89984";
+    whiteb = "#f2efed";
+  };
   solarized = {
     foreground = "#93a1a1";
     background = "#002b36";
