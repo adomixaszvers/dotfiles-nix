5b7705b60d4f9e95edd18bb0e52f11c26d33640b
Adomas Jatužis <adomixaszvers@gmail.com>
2020-04-21 15:42:39 +0300

    bspwm: use module

--- wm/bspwm/default.nix
+++ wm/bspwm/default.nix
@@ -1,6 +1,6 @@
 { pkgs, config, ... }: {
   imports = [ ../polybar.nix ../dunst.nix ../picom.nix ./sxhkd.nix ];
-  home.packages = with pkgs; [ bspwm mine.bspwm-reorder-desktops wmname tdrop ];
+  home.packages = with pkgs; [ bspwm mine.bspwm-reorder-desktops tdrop ];
   services.polybar.config = let
     common = {
       modules-left = "bspwm divider title";
@@ -42,10 +42,37 @@
       label-marked = "%{T2}M%{T-}";
     };
   };
-  xdg.configFile."bspwm/bspwmrc" = {
-    source = ./bspwmrc;
-    executable = true;
-    onChange = "pidof bspwm 1>/dev/null && bspc wm -r";
+  xsession.windowManager.bspwm = {
+    enable = true;
+    extraConfig = ''
+      feh --bg-max --image-bg white --no-fehbg ~/wallpaper.png
+      systemctl --user restart polybar.service
+    '';
+    monitors = { "'^1'" = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "0" ]; };
+    rules = {
+      Google-chrome = { desktop = "1"; };
+      Firefox = { desktop = "1"; };
+      jetbrains-idea = { desktop = "3"; };
+      rambox = { desktop = "4"; };
+      SmartGit = { desktop = "5"; };
+      Steam = { desktop = "5"; };
+      libreoffice = { desktop = "6"; };
+      KeePassXC = { desktop = "9"; };
+      "'Google Play Music Desktop Player'" = { desktop = "10"; };
+      Spotify = { desktop = "10"; };
+      "'*:scratchpad'" = { state = "floating"; };
+    };
+    settings = {
+      border_width = 2;
+      borderless_monocle = true;
+      focus_follows_pointer = true;
+      focused_border_color = config.colors.cyanb;
+      gapless_monocle = true;
+      pointer_follows_monitor = true;
+      single_monocle = true;
+      split_ratio = 0.52;
+      top_padding = 16;
+      window_gap = 7;
+    };
   };
-  xsession.windowManager.command = "bspwm";
 }
