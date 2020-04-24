e1fa8e0e2aa102ec57d6fa260ca2cd13e019db02
Adomas Jatužis <adomixaszvers@gmail.com>
2020-04-24 09:55:44 +0300

    update install script

--- install-home-manager.sh
+++ install-home-manager.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env sh
-HM_PATH=https://github.com/rycee/home-manager/archive/release-19.09.tar.gz
+
+RELEASE=20.03
+HM_PATH=https://github.com/rycee/home-manager/archive/release-${RELEASE}.tar.gz
 nix-channel --add $HM_PATH home-manager && \
     nix-channel --update home-manager && \
     nix-shell -I home-manager="$HOME/.nix-defexpr/channels/home-manager" '<home-manager>' -A install
