2958d672529012ac94463ec5f0711d1805c04bba
Adomas Jatužis <adomixaszvers@gmail.com>
2020-05-14 23:16:25 +0300

    kakoune: use wrapKakoune

--- cli/kakoune/default.nix
+++ cli/kakoune/default.nix
@@ -1,18 +1,16 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [
-    (kakoune.override {
-      configure.plugins = with kakounePlugins;
-        [ kak-fzf ] ++ [
-          (callPackage (import ./kakoune-text-objects.nix) { })
-          (callPackage (import ./sudo-write.nix) { })
-        ];
-    })
-    kak-lsp
-    mine.kaknix
-  ];
-  xdg.configFile."kak" = {
-    source = ./kak;
-    recursive = true;
-  };
+  home.packages = with pkgs;
+    let
+      kakouneTextObjects = callPackage (import ./kakoune-text-objects.nix) { };
+      sudoWrite = callPackage (import ./sudo-write.nix) { };
+      kakrc = runCommandNoCCLocal "kakrc" { } ''
+        mkdir -p $out/share/kak/autoload
+        cp ${./kakrc.kak} $out/share/kak/autoload/kakrc.kak
+      '';
+      myKakoune = wrapKakoune kakoune-unwrapped {
+        configure.plugins =
+          [ kakounePlugins.kak-fzf kakouneTextObjects sudoWrite kakrc ];
+      };
+    in [ myKakoune kak-lsp mine.kaknix ];
   xdg.configFile."kak-lsp/kak-lsp.toml".source = ./kak-lsp.toml;
 }
