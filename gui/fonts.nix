d96421b97e13af927801c565918b39fc79f43934
Adomas Jatužis <adomixaszvers@gmail.com>
2020-05-18 19:54:36 +0300

    fonts: use nerdfonts from nixos-unstable

--- gui/fonts.nix
+++ gui/fonts.nix
@@ -1,11 +1,13 @@
 { config, lib, pkgs, ... }:
 
 {
-  home.packages = with pkgs; [
-    corefonts
-    google-fonts
-    material-icons
-    nerdfonts
-    noto-fonts-emoji
-  ];
+  home.packages = with pkgs;
+    let unstable = pkgs.channels.nixos-unstable;
+    in [
+      corefonts
+      google-fonts
+      material-icons
+      unstable.nerdfonts
+      noto-fonts-emoji
+    ];
 }
