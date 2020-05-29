660fac7c68d2a634f1f878a6168f9315f504a252
Adomas Jatužis <adomixaszvers@gmail.com>
2020-05-29 05:47:01 +0300

    vimgolf: update dependencies

--- pkgs/vimgolf/gemset.nix
+++ pkgs/vimgolf/gemset.nix
@@ -1,32 +1,38 @@
 {
   highline = {
+    groups = ["default"];
+    platforms = [];
     source = {
-      remotes = [ "https://rubygems.org" ];
-      sha256 = "01ib7jp85xjc4gh4jg0wyzllm46hwv8p0w1m4c75pbgi41fps50y";
+      remotes = ["https://rubygems.org"];
+      sha256 = "0yclf57n2j3cw8144ania99h1zinf8q3f5zrhqa754j6gl95rp9d";
       type = "gem";
     };
-    version = "1.7.10";
+    version = "2.0.3";
   };
   json_pure = {
+    groups = ["default"];
+    platforms = [];
     source = {
-      remotes = [ "https://rubygems.org" ];
-      sha256 = "12yf9fmhr4c2jm3xl20vf1qyz5i63vc8a6ngz9j0f86nqwhmi2as";
+      remotes = ["https://rubygems.org"];
+      sha256 = "0kd19s22clqfbjdj6ni1sv7bwmrfdqbcdaa52vi1djmcpnib48wx";
       type = "gem";
     };
-    version = "2.1.0";
+    version = "2.3.0";
   };
   thor = {
+    groups = ["default"];
+    platforms = [];
     source = {
-      remotes = [ "https://rubygems.org" ];
-      sha256 = "0nmqpyj642sk4g16nkbq6pj856adpv91lp4krwhqkh2iw63aszdl";
+      remotes = ["https://rubygems.org"];
+      sha256 = "1xbhkmyhlxwzshaqa7swy2bx6vd64mm0wrr8g3jywvxy7hg0cwkm";
       type = "gem";
     };
-    version = "0.20.0";
+    version = "1.0.1";
   };
   vimgolf = {
-    dependencies = [ "highline" "json_pure" "thor" ];
+    dependencies = ["highline" "json_pure" "thor"];
     source = {
-      remotes = [ "https://rubygems.org" ];
+      remotes = ["https://rubygems.org"];
       sha256 = "1qp6fdycxb65ivi3szb9d9xh2cvv36bpkcw3k0a2ggg27l9afbq4";
       type = "gem";
     };
