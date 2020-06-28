62e0bf9b676cee95bf99bec3eb5327ebbfa8b1ae
Adomas Jatužis <adomixaszvers@gmail.com>
2020-06-29 00:06:16 +0300

    xmonad: use all-hies overlay

---
+++ overlays/hie.nix
@@ -0,0 +1,1 @@
+import <all-hies/overlay.nix>
--- wm/xmonad/shell.nix
+++ wm/xmonad/shell.nix
@@ -1,14 +1,12 @@
 { pkgs ? import <nixpkgs> { } }:
 let
   extraPackages = import ./extraPackages.nix;
-  all-hies = import <all-hies> { };
-  hie = all-hies.selection { selector = p: { inherit (p) ghc865; }; };
 in with pkgs;
 
 mkShell {
   buildInputs = [
     (ghc.withHoogle extraPackages)
-    hie
+    haskellPackages.hie
     haskellPackages.brittany
     haskellPackages.hlint
   ];
