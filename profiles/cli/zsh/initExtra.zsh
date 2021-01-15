68d96a6a6a88b705de3514b54e70100253dd6a5d
Adomas Jatužis <adomixaszvers@gmail.com>
2021-01-15 15:21:35 +0200

    fix multiselect in "he" function

--- profiles/cli/zsh/initExtra.zsh
+++ profiles/cli/zsh/initExtra.zsh
@@ -1,11 +1,11 @@
 find-shells () {
-  cat $XDG_DATA_HOME/direnv/allow/* | sort | uniq | sed -e 's/\.envrc$/shell.nix/'
+  cat "$XDG_DATA_HOME"/direnv/allow/* | sort | uniq | sed -e 's/\.envrc$/shell.nix/'
 }
 hcd () {
-  cd ~/.config/nixpkgs
+  cd ~/.config/nixpkgs || exit 1
 }
 he () {
   (
-    hcd && local FILES="$(fzf)" && [ ! -z "$FILES" ] && $EDITOR $FILES
+    hcd && local FILES=("${(@f)$(fzf --multi)}") && [ "${#FILES[@]}" -ne 0 ] && "$EDITOR" "${FILES[@]}"
   )
 }
