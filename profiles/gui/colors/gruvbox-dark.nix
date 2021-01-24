a2a6fac14f3573a5acb35ae7e2ee1bcdf56a0a6d
Adomas Jatužis <adomixaszvers@gmail.com>
2021-01-24 13:44:05 +0200

    add gruvbox colors

---
+++ profiles/gui/colors/gruvbox-dark.nix
@@ -0,0 +1,31 @@
+{
+  cursorColor = "#ebdbb2";
+  # hard contrast: *background: #1d2021
+  background = "#282828";
+  # soft contrast: *background: #32302f
+  foreground = "#ebdbb2";
+  # Black + DarkGrey
+  black = "#282828";
+  blackb = "#928374";
+  # DarkRed + Red
+  red = "#cc241d";
+  redb = "#fb4934";
+  # DarkGreen + Green
+  green = "#98971a";
+  greenb = "#b8bb26";
+  # DarkYellow + Yellow
+  yellow = "#d79921";
+  yellowb = "#fabd2f";
+  # DarkBlue + Blue
+  blue = "#458588";
+  blueb = "#83a598";
+  # DarkMagenta + Magenta
+  magenta = "#b16286";
+  magentab = "#d3869b";
+  # DarkCyan + Cyan
+  cyan = "#689d6a";
+  cyanb = "#8ec07c";
+  # LightGrey + White
+  white = "#a89984";
+  whiteb = "#ebdbb2";
+}
