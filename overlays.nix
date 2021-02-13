94a01128356e003f86fac2a9fe5f4dfd3d6a4ecd
Adomas Jatužis <adomixaszvers@gmail.com>
2021-02-13 23:23:31 +0200

    remove hie from overlays

--- overlays.nix
+++ overlays.nix
@@ -1,16 +1,15 @@
 let
   sources = import ./nix/sources.nix;
-  hie = import "${sources.all-hies}/overlay.nix";
   mine = import ./pkgs/overlay.nix;
   nivSources = _: _: { nivSources = sources; };
   gitignoreSource = _: super:
     let gitignore = (import sources.gitignore) { inherit (super) lib; };
     in { inherit (gitignore) gitignoreSource; };
   nixos-unstable = _: _: {
-    nixos-unstable = import sources.nixos-unstable { overlays = [ mine hie ]; };
+    nixos-unstable = import sources.nixos-unstable { overlays = [ mine ]; };
   };
   overlay = _: _:
     import sources.nixpkgs {
-      overlays = [ mine hie nixos-unstable nivSources gitignoreSource ];
+      overlays = [ mine nixos-unstable nivSources gitignoreSource ];
     };
 in [ overlay ]
