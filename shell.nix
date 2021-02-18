0f57c1248e449e48c05da8d4c5c664d47e64d7a0
Adomas Jatužis <adomixaszvers@gmail.com>
2021-02-18 23:06:48 +0200

    add git-crypt to shell

--- shell.nix
+++ shell.nix
@@ -2,6 +2,7 @@ let
   nivSources = import ./nix/sources.nix;
   pkgs = import nivSources.nixpkgs { };
 in pkgs.mkShell {
+  buildInputs = with pkgs; [ git-crypt ];
   shellHook = ''
     ${(import ./default.nix).pre-commit-check.shellHook}
   '';
