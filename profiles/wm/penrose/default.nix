d3f5cb555ffe061c9c4ddb9b071e8fe33a4c7d55
Adomas Jatužis <adomixaszvers@gmail.com>
2021-02-21 00:33:23 +0200

    penrose: rename some files

--- profiles/wm/penrose/default.nix
+++ profiles/wm/penrose/default.nix
@@ -2,6 +2,6 @@
   imports = [ ../picom.nix ../dunst.nix ../polybar.nix ];
   home.packages = with pkgs; [ dmenu xorg.xmodmap ];
   xsession.windowManager.command =
-    let package = pkgs.callPackage ./my-penrose-config { };
+    let package = pkgs.callPackage ./my-penrose-config/derivation.nix { };
     in "${package}/bin/my_penrose_config";
 }
