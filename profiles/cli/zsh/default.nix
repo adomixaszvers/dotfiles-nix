186d803f86e10c4e30fdb8d6b8daa9c64460a9b5
Adomas Jatužis <adomixaszvers@gmail.com>
2021-02-28 11:18:30 +0200

    zsh: fix emacs tramp handing

--- profiles/cli/zsh/default.nix
+++ profiles/cli/zsh/default.nix
@@ -1,4 +1,9 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
+  home.file.".zshrc".text = lib.mkBefore ''
+    # fix hanging on emacs tramp
+    # see: https://www.emacswiki.org/emacs/TrampMode#h5o-9
+    [[ $TERM == "dumb" ]] && unsetopt zle && PS1='$ ' && return
+  '';
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
