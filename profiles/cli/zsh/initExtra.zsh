bbe9c8f71c7ba73488490443d9920e898d5e0cfd
Adomas Jatužis <adomixaszvers@gmail.com>
2021-03-03 21:25:04 +0200

    zsh: add some keybindings

--- profiles/cli/zsh/initExtra.zsh
+++ profiles/cli/zsh/initExtra.zsh
@@ -9,3 +9,17 @@ he () {
     hcd && local FILES=("${(@f)$(fzf --multi)}") && [ -n "${FILES[*]}" ] && "$EDITOR" "${FILES[@]}"
   )
 }
+
+# enable completion menu
+zstyle ':completion:*' menu select
+zmodload zsh/complist
+# enable edit command line
+autoload edit-command-line; zle -N edit-command-line 
+
+# Use vim keys in tab complete menu:
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
+bindkey -v '^?' backward-delete-char
+bindkey '^e' edit-command-line
