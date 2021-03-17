5f232fe4abe5b0be17a8c8c95f701b6230378aec
Adomas Jatužis <adomixaszvers@gmail.com>
2021-03-17 23:06:14 +0200

    add nixpkgs and nixos-unstable to flake-registry
    it's is a hack-ish way but it works for commands like `nix run nixpkgs#vim`

--- nixos/flakes.nix
+++ nixos/flakes.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
-let nixPackage = pkgs.nixFlakes;
+let
+  nixPackage = pkgs.nixFlakes;
+  flake-compat = builtins.fetchTarball
+    "https://github.com/edolstra/flake-compat/archive/master.tar.gz";
+  adomoFlakes = import flake-compat { src = /home/adomas/.config/nixpkgs; };
 in {
   nix = {
     extraOptions = ''
       experimental-features = nix-command flakes ca-references
     '';
     package = nixPackage;
+    registry = with adomoFlakes.defaultNix.inputs; {
+      nixpkgs.flake = nixpkgs;
+      nixos-unstable.flake = nixos-unstable;
+    };
   };
   programs.zsh.interactiveShellInit = ''
     source ${nixPackage.src}/misc/zsh/completion.zsh
