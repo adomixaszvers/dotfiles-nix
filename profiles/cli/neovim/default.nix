88f67387964d00325a81ba7269bf91edae86cd16
Adomas Jatužis <adomixaszvers@gmail.com>
2021-07-23 07:17:15 +0300

    neovim: update to v0.5

--- profiles/cli/neovim/default.nix
+++ profiles/cli/neovim/default.nix
@@ -1,14 +1,38 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+let unstable = pkgs.nixos-unstable;
+in with unstable; {
   programs.neovim = {
     enable = true;
+    package = neovim-unwrapped;
     extraConfig = lib.readFile ./vimrc;
-    plugins = with pkgs.vimPlugins; [
+    plugins = with vimPlugins; [
       ale
       commentary
       fugitive
       fzf-vim
       fzfWrapper
       neoformat
+      {
+        plugin = nvim-treesitter.withPlugins (_: tree-sitter.allGrammars);
+        config = ''
+          lua <<EOF
+            require'nvim-treesitter.configs'.setup {
+              highlight = {
+                enable = true,              -- false will disable the whole extension
+              },
+              incremental_selection = {
+                enable = true,
+                keymaps = {
+                  init_selection = "<leader>gn",
+                  node_incremental = "grn",
+                  scope_incremental = "grc",
+                  node_decremental = "grm",
+                },
+              },
+            }
+          EOF
+        '';
+      }
       rainbow
       repeat
       {
