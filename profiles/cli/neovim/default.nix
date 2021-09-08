c72e655bf60acba1e4d30f7bd8bb1d8d2442ca31
Adomas Jatužis <adomixaszvers@gmail.com>
2021-09-08 13:58:49 +0300

    neovim: use telescope.vim

--- profiles/cli/neovim/default.nix
+++ profiles/cli/neovim/default.nix
@@ -5,53 +5,53 @@ in {
     enable = true;
     package = neovim-unwrapped;
     extraConfig = lib.readFile ./vimrc;
-    plugins = with vimPlugins; [
-      ale
-      commentary
-      fugitive
-      fzf-vim
-      fzfWrapper
-      neoformat
-      {
-        plugin = nvim-treesitter.withPlugins (_: tree-sitter.allGrammars);
-        config = ''
-          lua <<EOF
-            require'nvim-treesitter.configs'.setup {
-              highlight = {
-                enable = true,              -- false will disable the whole extension
-              },
-              incremental_selection = {
-                enable = true,
-                keymaps = {
-                  init_selection = "<leader>gn",
-                  node_incremental = "grn",
-                  scope_incremental = "grc",
-                  node_decremental = "grm",
+    plugins = with vimPlugins;
+      let telescope-dependencies = [ plenary-nvim telescope-nvim ];
+      in [
+        ale
+        commentary
+        fugitive
+        neoformat
+        {
+          plugin = nvim-treesitter.withPlugins (_: tree-sitter.allGrammars);
+          config = ''
+            lua <<EOF
+              require'nvim-treesitter.configs'.setup {
+                highlight = {
+                  enable = true,              -- false will disable the whole extension
                 },
-              },
-            }
-          EOF
-        '';
-      }
-      {
-        plugin = playground;
-        config = builtins.readFile ./playground.vim;
-      }
-      rainbow
-      repeat
-      {
-        plugin = suda-vim;
-        config = "let g:suda_smart_edit = 1";
-      }
-      surround
-      vim-airline
-      vim-airline-themes
-      vim-colorschemes
-      vim-gitgutter
-      vim-polyglot
-      vim-sneak
-      vim-unimpaired
-      vinegar
-    ];
+                incremental_selection = {
+                  enable = true,
+                  keymaps = {
+                    init_selection = "<leader>gn",
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                  },
+                },
+              }
+            EOF
+          '';
+        }
+        {
+          plugin = playground;
+          config = builtins.readFile ./playground.vim;
+        }
+        rainbow
+        repeat
+        {
+          plugin = suda-vim;
+          config = "let g:suda_smart_edit = 1";
+        }
+        surround
+        vim-airline
+        vim-airline-themes
+        vim-colorschemes
+        vim-gitgutter
+        vim-polyglot
+        vim-sneak
+        vim-unimpaired
+        vinegar
+      ] ++ telescope-dependencies;
   };
 }
