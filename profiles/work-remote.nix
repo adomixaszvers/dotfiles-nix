a5c7de1d8db1538e585068a7d58a4c9b36a1fb24
Adomas Jatužis <adomixaszvers@gmail.com>
2021-09-24 11:16:09 +0300

    bspwm: adopt orphans upon restart

--- profiles/work-remote.nix
+++ profiles/work-remote.nix
@@ -18,9 +18,10 @@
   services.gpg-agent.enable = lib.mkForce false;
   services.picom.enable = false;
   services.screen-locker.enable = lib.mkForce false;
-  services.sxhkd.keybindings = { "super + ctrl + r" = "bspc wm -r"; };
+  services.sxhkd.keybindings = {
+    "super + ctrl + r" = "bspc wm -r; bspc wm -o";
+  };
   xsession.windowManager.bspwm.monitors = {
     "rdp0" = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" ];
-    "MONITOR" = [ "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" ];
   };
 }
