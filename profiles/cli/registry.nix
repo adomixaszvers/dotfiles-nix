feebb56986b0e7e4b4d7fe3b21ba7aea304f3fd9
Adomas Jatužis <adomixaszvers@gmail.com>
2021-10-22 00:00:56 +0300

    avoid constant user nix registry changes

--- profiles/cli/registry.nix
+++ profiles/cli/registry.nix
@@ -1,6 +1,9 @@
 { inputs, ... }: {
   nix.registry = with inputs; {
-    mine.flake = self;
+    mine.to = {
+      type = "git";
+      url = "file:///home/adomas/.config/nixpkgs";
+    };
     nixpkgs.flake = nixpkgs;
     nixos-unstable.flake = nixos-unstable;
   };
