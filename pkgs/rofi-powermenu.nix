3c7a02425484f400373693624c91a7d1a8445be6
Adomas Jatužis <adomixaszvers@gmail.com>
2021-12-03 23:25:08 +0200

    rofi-powermenu: add confirmations

--- pkgs/rofi-powermenu.nix
+++ pkgs/rofi-powermenu.nix
@@ -1,10 +1,10 @@
-{ writeShellScriptBin, rofi, lib }:
+{ writeShellScriptBin, gnome, rofi, lib }:
 writeShellScriptBin "rofi-powermenu" ''
-  PATH=$PATH:${lib.makeBinPath [ rofi ]}
+  PATH=$PATH:${lib.makeBinPath [ rofi gnome.zenity ]}
   case "$(printf "lock session\\nlogout\\npoweroff\\nreboot"| rofi -dmenu -l 4 -i -p "Power menu")" in
     "lock session") loginctl lock-session ;;
-    "logout") loginctl kill-session $XDG_SESSION_ID ;;
-    "poweroff") systemctl poweroff ;;
-    "reboot") systemctl reboot ;;
+    "logout") zenity --question --text="Are you sure you want to logout?" && loginctl kill-session $XDG_SESSION_ID ;;
+    "poweroff") zenity --question --text="Are you sure you want to power off the computer?" && systemctl poweroff ;;
+    "reboot") zenity --question --text="Are you sure you want to reboot the computer?" && systemctl reboot ;;
   esac
 ''
