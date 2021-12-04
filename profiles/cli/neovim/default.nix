3c26740a2ab3f8bd9ad35157dd02d9282e99237e
Adomas Jatužis <adomixaszvers@gmail.com>
2021-12-04 22:40:06 +0200

    neovim: use home-manager options again

--- profiles/cli/neovim/default.nix
+++ profiles/cli/neovim/default.nix
@@ -1,37 +1,28 @@
-{ pkgs, lib, ... }:
-let
-  inherit (pkgs.nixos-unstable)
-    vimPlugins tree-sitter neovim-unwrapped wrapNeovimUnstable neovimUtils;
-  myNeovimConfig = neovimUtils.makeNeovimConfig {
-    configure = {
-      packages.myPackageDir.start = with vimPlugins;
-        let telescope-dependencies = [ plenary-nvim telescope-nvim ];
-        in [
-          ale
-          commentary
-          fugitive
-          neoformat
-          (nvim-treesitter.withPlugins (_: tree-sitter.allGrammars))
-          playground
-          rainbow
-          repeat
-          suda-vim
-          surround
-          vim-airline
-          vim-airline-themes
-          vim-colorschemes
-          vim-gitgutter
-          vim-polyglot
-          vim-sneak
-          vim-unimpaired
-          vinegar
-        ] ++ telescope-dependencies;
-      customRC = lib.readFile ./init.vim + lib.readFile ./playground.vim;
-    };
-    wrapRc = false;
+{ pkgs, lib, ... }: {
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins;
+      let telescope-dependencies = [ plenary-nvim telescope-nvim ];
+      in [
+        ale
+        commentary
+        fugitive
+        neoformat
+        (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
+        playground
+        rainbow
+        repeat
+        suda-vim
+        surround
+        vim-airline
+        vim-airline-themes
+        vim-colorschemes
+        vim-gitgutter
+        vim-polyglot
+        vim-sneak
+        vim-unimpaired
+        vinegar
+      ] ++ telescope-dependencies;
+    extraConfig = lib.readFile ./init.vim + lib.readFile ./playground.vim;
   };
-  myNeovim = wrapNeovimUnstable neovim-unwrapped myNeovimConfig;
-in {
-  home.packages = [ myNeovim ];
-  xdg.configFile."nvim/init.vim".text = myNeovimConfig.neovimRcContent;
 }
