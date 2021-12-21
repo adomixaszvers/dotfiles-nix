2488be2ab440d3a48f12898bec6456b8c4aa2926
Adomas Jatužis <adomixaszvers@gmail.com>
2021-12-21 22:36:37 +0200

    nixos: update wakeonlan settings

--- nixos/home/wakeonlan.nix
+++ nixos/home/wakeonlan.nix
@@ -1,8 +1,5 @@
 { pkgs, ... }: {
-  services.wakeonlan.interfaces = [{
-    interface = "enp5s0";
-    method = "magicpacket";
-  }];
+  networking.interfaces.enp5s0.wakeOnLan.enable = true;
   services.cron.enable = true;
   services.cron.systemCronJobs =
     [ "@reboot root ${pkgs.ethtool}/sbin/ethtool -s enp5s0 wol g" ];
