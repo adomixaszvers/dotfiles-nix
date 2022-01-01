fb628c6ec3448b836957fe54edf81854d1a82fcb
Adomas Jatužis <adomixaszvers@gmail.com>
2022-01-01 09:03:00 +0200

    rofi-powermenu: drop zenity

--- pkgs/rofi-powermenu.nix
+++ pkgs/rofi-powermenu.nix
@@ -1,10 +1,16 @@
-{ writeShellScriptBin, gnome, rofi, lib }:
+{ writeShellScriptBin, rofi, lib }:
 writeShellScriptBin "rofi-powermenu" ''
-  PATH=$PATH:${lib.makeBinPath [ rofi gnome.zenity ]}
+  PATH=$PATH:${lib.makeBinPath [ rofi ]}
+
+  rofi-question () {
+    local question=$1
+    [ $(printf "No\\nYes" | rofi -dmenu -l 2 -i -disable-history -p "$question") == Yes ]
+  }
+
   case "$(printf "lock session\\nlogout\\npoweroff\\nreboot"| rofi -dmenu -l 4 -i -p "Power menu")" in
     "lock session") loginctl lock-session ;;
-    "logout") zenity --question --text="Are you sure you want to logout?" && loginctl kill-session $XDG_SESSION_ID ;;
-    "poweroff") zenity --question --text="Are you sure you want to power off the computer?" && systemctl poweroff ;;
-    "reboot") zenity --question --text="Are you sure you want to reboot the computer?" && systemctl reboot ;;
+    "logout") rofi-question "Are you sure you want to logout?" && loginctl kill-session $XDG_SESSION_ID ;;
+    "poweroff") rofi-question "Are you sure you want to power off the computer?" && systemctl poweroff ;;
+    "reboot") rofi-question "Are you sure you want to reboot the computer?" && systemctl reboot ;;
   esac
 ''
