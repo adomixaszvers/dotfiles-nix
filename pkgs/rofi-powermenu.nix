bc10a3310f9ee0da901d5c457b413dbf08fdb5c4
Adomas Jatužis <adomixaszvers@gmail.com>
2022-01-04 23:28:45 +0200

    rofi-powermenu: use `self` as session id

--- pkgs/rofi-powermenu.nix
+++ pkgs/rofi-powermenu.nix
@@ -9,7 +9,7 @@ writeShellScriptBin "rofi-powermenu" ''
 
   case "$(printf "lock session\\nlogout\\npoweroff\\nreboot"| rofi -dmenu -l 4 -i -p "Power menu")" in
     "lock session") loginctl lock-session ;;
-    "logout") rofi-question "Are you sure you want to logout?" && loginctl kill-session $XDG_SESSION_ID ;;
+    "logout") rofi-question "Are you sure you want to logout?" && loginctl kill-session self ;;
     "poweroff") rofi-question "Are you sure you want to power off the computer?" && systemctl poweroff ;;
     "reboot") rofi-question "Are you sure you want to reboot the computer?" && systemctl reboot ;;
   esac
