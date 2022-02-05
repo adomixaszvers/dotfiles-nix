c2871dfdefb1e86fe03283eb53a052d84e927a52
Adomas Jatužis <adomixaszvers@gmail.com>
2022-02-05 23:11:09 +0200

    fail2ban: harden rules

--- nixos/pi/fail2ban.nix
+++ nixos/pi/fail2ban.nix
@@ -2,6 +2,16 @@
   services.fail2ban = {
     enable = true;
     ignoreIP = [ "192.168.0.0/16" "10.6.0.0/24" "10.147.17.0/24" ];
+    jails = {
+      sshd = ''
+        enabled   = true
+        filter    = sshd
+        banaction = iptables
+        backend   = systemd
+        maxretry  = 5
+        findtime  = 1d
+        bantime   = 2w
+      '';
+    };
   };
 }
-
