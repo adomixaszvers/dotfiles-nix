f9c3690824472859cf7cf39e18ea8962ba45b0bf
Adomas Jatužis <adomixaszvers@gmail.com>
2022-02-13 15:26:26 +0200

    nvim-treesitter: enable markdown again

--- profiles/cli/neovim/nvim-treesitter.vim
+++ profiles/cli/neovim/nvim-treesitter.vim
@@ -2,7 +2,6 @@ lua <<EOF
     require'nvim-treesitter.configs'.setup {
         highlight = {
             enable = true,              -- false will disable the whole extension
-            disable = { "markdown", },  -- markdown grammar currently crashes
         },
         incremental_selection = {
             enable = true,
--- profiles/cli/neovim/package.nix
+++ profiles/cli/neovim/package.nix
@@ -1,14 +1,13 @@
-{ neovimUtils, lib, wrapNeovimUnstable, neovim-unwrapped, pkgs }:
+{ neovimUtils, wrapNeovimUnstable, neovim-unwrapped, pkgs }:
 let
   plugins = import ./plugins.nix pkgs;
   moduleConfigure = {
-    packages.home-manager = {
-      start = lib.remove null (map
-        (x: if x ? plugin && x.optional or false then null else (x.plugin or x))
-        plugins);
-      opt = lib.remove null
-        (map (x: if x ? plugin && x.optional or false then x.plugin else null)
-          plugins);
+    packages.home-manager = let
+      ps = builtins.partition (x: x.optional or false)
+        (map (x: x.plugin or x) plugins);
+    in {
+      start = ps.wrong;
+      opt = ps.right;
     };
     beforePlugins = "";
   };
