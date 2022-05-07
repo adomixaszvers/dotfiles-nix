6801fdb63702dc52e5dac2664bc2e0658762949d
Adomas Jatužis <adomixaszvers@gmail.com>
2022-05-07 10:30:09 +0300

    mount insoft storage as rw for some cases

--- nixos/work/samba.nix
+++ nixos/work/samba.nix
@@ -1,5 +1,5 @@
-{
-  fileSystems."/kiti/insoft" = {
+let
+  mkMount = rw: {
     device = "//disk.insoft.dev/storage";
     fsType = "cifs";
     options = let
@@ -7,7 +7,14 @@
       automount_opts =
         "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000,gid=100";
 
-    in [ "${automount_opts},ro,credentials=/root/smb-secrets" ];
+    in [
+      "${automount_opts},${
+        if rw then "rw" else "ro"
+      },credentials=/root/smb-secrets"
+    ];
     noCheck = true;
   };
+in {
+  fileSystems."/kiti/insoft" = mkMount false;
+  fileSystems."/kiti/insoft_rw" = mkMount true;
 }
