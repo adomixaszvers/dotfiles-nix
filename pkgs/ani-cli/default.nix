9f01a8d4da992bf220767040c2f2a07ffcec4ced
Adomas Jatužis <adomixaszvers@gmail.com>
2022-05-15 17:07:07 +0300

    fix ani-cli

--- pkgs/ani-cli/default.nix
+++ pkgs/ani-cli/default.nix
@@ -1,9 +1,11 @@
 { ani-cli, bash, lib, gnugrep, gnused, ffmpeg, mpv, curl, runCommandLocal
-, makeWrapper }:
+, openssl, makeWrapper }:
 runCommandLocal "ani-cli" {
   script = "${ani-cli}/ani-cli";
   nativeBuildInputs = [ makeWrapper ];
 } ''
   makeWrapper $script $out/bin/ani-cli \
-    --prefix PATH : ${lib.makeBinPath [ bash gnugrep gnused ffmpeg mpv curl ]}
+    --prefix PATH : ${
+      lib.makeBinPath [ bash gnugrep gnused ffmpeg mpv curl openssl ]
+    }
 ''
