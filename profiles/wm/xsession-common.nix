4d1ecd7ee41ae4ae6c7b3254a8f58024f0f89a6a
Adomas Jatužis <adomixaszvers@gmail.com>
2022-06-07 10:32:41 +0300

    enable dpms on lockscreen

--- profiles/wm/xsession-common.nix
+++ profiles/wm/xsession-common.nix
@@ -6,9 +6,20 @@
     timeout = 10;
   };
   services.screen-locker = {
-    lockCmd = lib.mkDefault "${pkgs.i3lock}/bin/i3lock -n -t -c ${
-        builtins.substring 1 6 config.colors.background
-      } -f";
+    lockCmd = let
+      dpmsTimeoutSeconds = 10;
+      lockScreen = pkgs.writers.writeDash "lock-screen.sh" ''
+        PATH=${lib.makeBinPath [ pkgs.i3lock pkgs.xorg.xset ]}:$PATH
+        DPMS_TIMEOUT=${toString dpmsTimeoutSeconds}
+        revert() {
+          xset dpms 0 0 0
+        }
+        trap revert HUP INT TERM
+        xset +dpms dpms $DPMS_TIMEOUT $DPMS_TIMEOUT $DPMS_TIMEOUT
+        i3lock -n -t -c ${builtins.substring 1 6 config.colors.background} -f
+        revert
+      '';
+    in lib.mkDefault lockScreen.outPath;
     xautolock.extraOptions = [ "-corners" "--00" ];
   };
   xsession.enable = lib.mkDefault true;
