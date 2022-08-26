d7b6ebe47c29a0ea08ea09f7b16ae93fdca4f1ea
Adomas Jatužis <adomixaszvers@gmail.com>
2022-08-26 05:12:07 +0300

    update gnome config

--- nixos/gnome.nix
+++ nixos/gnome.nix
@@ -1,6 +1,7 @@
 {
+  environment.sessionVariables.MOZ_ENABLE_WAYLAND = "1";
   services.xserver = {
     displayManager.gdm.enable = true;
-    desktopManager.gnome3.enable = true;
+    desktopManager.gnome.enable = true;
   };
 }
