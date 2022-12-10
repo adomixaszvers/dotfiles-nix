f51bb596baa0c81e07a25d87c6ae339721c85f29
Adomas Jatužis <adomixaszvers@gmail.com>
2022-12-10 08:55:30 +0200

    enable `repl-flake`

--- nixos/flakes.nix
+++ nixos/flakes.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   nix = {
     extraOptions = ''
-      experimental-features = nix-command flakes
+      experimental-features = nix-command flakes repl-flake
     '';
     package = pkgs.nixFlakes;
   };
