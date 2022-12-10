348345c17b4084eabee6343c83f192133b7f2b93
Adomas Jatužis <adomixaszvers@gmail.com>
2022-12-10 08:54:59 +0200

    work: some remoting conveniance

--- nixos/work/configuration.nix
+++ nixos/work/configuration.nix
@@ -156,11 +156,16 @@
     passwordAuthentication = false;
     ports = [ 9222 22 ];
   };
+  security.pam.services.sshd.enableGnomeKeyring = true;
   services.printing.enable = true;
   services.vnstat.enable = true;
 
   services.xrdp.enable = true;
   services.xrdp.package = inputs.self.packages.x86_64-linux.custom-xrdp;
+  security.pam.services.xrdp-sesman = {
+    enableGnomeKeyring = true;
+    fprintAuth = false;
+  };
   networking.firewall.interfaces.ztzlgoe57z.allowedTCPPorts = [ 3389 ];
 
   hardware.opengl.driSupport32Bit = true;
