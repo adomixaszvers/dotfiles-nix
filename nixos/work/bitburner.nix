9415a4276c831dde0794bad6803bd99b4aa7e429
Adomas Jatužis <adomixaszvers@gmail.com>
2023-02-02 09:42:30 +0200

    bitburner: use a separate user

--- nixos/work/bitburner.nix
+++ nixos/work/bitburner.nix
@@ -1,5 +1,29 @@
 { pkgs, ... }: {
+  nixpkgs.overlays = [
+    (self: super: {
+      libfakeXinerama = super.libfakeXinerama.overrideAttrs (_old: {
+        installPhase = ''
+          mkdir -p $out/lib
+          cp libfakeXinerama.so.1.0 $out/lib
+          ln -s libfakeXinerama.so.1.0 $out/lib/libXinerama.so.1.0
+          ln -s libfakeXinerama.so.1.0 $out/lib/libfakeXinerama.so.1
+          ln -s libXinerama.so.1.0 $out/lib/libXinerama.so.1
+          ln -s libXinerama.so.1 $out/lib/libXinerama.so
+        '';
+      });
+    })
+  ];
   environment.systemPackages = [ pkgs.xpra ];
+  users = {
+    users.bitburner = {
+      isSystemUser = true;
+      group = "bitburner";
+      home = "/var/lib/bitburner";
+      createHome = true;
+      description = "BitBurner daemon user";
+    };
+    groups.bitburner = { };
+  };
   systemd.services.bitburner = {
     path = with pkgs; [ xpra chromium ];
     after = [ "network.target" ];
@@ -12,8 +36,12 @@
         hash = "sha256-IGu4/GHHfE/d1OrG2d8Y5XDDTAlrSn/xbc1ory7mtAg=";
       };
     in ''
-      xpra start --printing=no --speaker=no --session-name=bitburner --no-daemon --exit-with-children --start-child="chromium --app=file://${bitburnerSrc}/index.html --user-data-dir=/home/adomas/.bitburner-profile"
+      xpra start :99 --bind-tcp=10.6.0.6:9292 --daemon=no --notifications=no --printing=no --speaker=no --session-name=bitburner --exit-with-children --start-child="chromium --app=file://${bitburnerSrc}/index.html --user-data-dir=/var/lib/bitburner/.bitburner-profile"
     '';
-    serviceConfig = { User = "adomas"; };
+    serviceConfig = {
+      User = "bitburner";
+      RestartSec = 60;
+      Restart = "always";
+    };
   };
 }
