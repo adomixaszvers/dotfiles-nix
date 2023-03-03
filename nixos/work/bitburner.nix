4b732a2f341036efec8f50df9cc052d5ca284cf5
Adomas Jatužis <adomixaszvers@gmail.com>
2023-03-03 13:49:56 +0200

    bump bitburner to 2.2.2

--- nixos/work/bitburner.nix
+++ nixos/work/bitburner.nix
@@ -31,12 +31,12 @@
     script = let
       bitburnerSrc = pkgs.fetchzip {
         url =
-          "https://github.com/bitburner-official/bitburner-src/releases/download/v2.2.1/Bitburner.v2.2.1.zip";
+          "https://github.com/bitburner-official/bitburner-src/releases/download/v2.2.2/Release2.2.2.zip";
         stripRoot = false;
-        hash = "sha256-IGu4/GHHfE/d1OrG2d8Y5XDDTAlrSn/xbc1ory7mtAg=";
+        hash = "sha256-tXchK+emZKNhjIyrNI9g0oUymRVKt5XTkSJYh6BFsNc=";
       };
     in ''
-      xpra start :99 --bind-tcp=10.6.0.6:9292 --daemon=no --notifications=no --printing=no --speaker=no --session-name=bitburner --exit-with-children --start-child="chromium --app=file://${bitburnerSrc}/index.html --user-data-dir=/var/lib/bitburner/.bitburner-profile"
+      xpra start :99 --bind-tcp=10.6.0.6:9292 --bind-tcp=127.0.0.1:9292 --daemon=no --notifications=no --printing=no --speaker=no --session-name=bitburner --exit-with-children --start-child="chromium --app=file://${bitburnerSrc}/index.html --user-data-dir=/var/lib/bitburner/.bitburner-profile"
     '';
     serviceConfig = {
       User = "bitburner";
