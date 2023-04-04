671f4f69a91b38bd0a04adf82fcc76216958598f
Adomas Jatužis <adomixaszvers@gmail.com>
2023-04-04 16:34:23 +0300

    bspwm-greedy-focus: add some comments

--- pkgs/bspwm-greedy-focus.nix
+++ pkgs/bspwm-greedy-focus.nix
@@ -9,12 +9,15 @@ writeShellScriptBin "bspwm-greedy-focus" ''
 
   desktop=$(bspc query -d "$1" -D) || exit_with_error "desktop $1 not found"
 
+  # the desktop is on focused monitor. we just focus it
   if bspc query -d "$desktop" -m focused -D; then
     bspc desktop -f "$desktop"
+  # the desktop is active on another monitor. we should swap
   elif bspc query -d "$desktop.active" -D; then
     focused_desktop=$(bspc query -d focused -D)
     bspc desktop "$desktop" -s "$focused_desktop"
     bspc desktop -a "$focused_desktop"
+  # otherwise we move the desktop
   else
     bspc desktop "$desktop" -m focused
     bspc desktop -f "$desktop"
