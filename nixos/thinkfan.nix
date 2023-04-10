6cc86587529843337d0fb0f81ae8385d7422ec34
Adomas Jatužis <adomixaszvers@gmail.com>
2023-04-10 13:24:38 +0300

    thinkfan: adjust levels by temperature

--- nixos/thinkfan.nix
+++ nixos/thinkfan.nix
@@ -1,12 +1,12 @@
-{
+{ lib, ... }: {
   boot.extraModprobeConfig = ''
     options thinkpad_acpi fan_control=1
   '';
   services.thinkfan = {
-    enable = true;
+    enable = lib.mkDefault true;
     levels = [
-      [ 0 0 61 ]
-      [ 1 60 66 ]
+      [ 0 0 56 ]
+      [ 1 55 66 ]
       [ 2 65 71 ]
       [ 3 70 76 ]
       [ 4 75 81 ]
