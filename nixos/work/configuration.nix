d6d72cbd860273096e2b599e91d199687b046b65
Adomas Jatužis <adomixaszvers@gmail.com>
2023-05-20 12:01:47 +0300

    work: move xrdp config to a module

--- nixos/work/configuration.nix
+++ nixos/work/configuration.nix
@@ -25,6 +25,7 @@
     ./ltXkb.nix
     ./prebuild-configs.nix
     ./samba.nix
+    ./xrdp
     ./turbovnc.nix
     # ./unbound.nix
     ./vnc.nix
@@ -162,14 +163,6 @@
   security.pam.services.sshd.enableGnomeKeyring = true;
   services.printing.enable = true;
   services.vnstat.enable = true;
-
-  services.xrdp.enable = true;
-  services.xrdp.package = inputs.self.packages.x86_64-linux.custom-xrdp;
-  security.pam.services.xrdp-sesman = {
-    enableGnomeKeyring = true;
-    fprintAuth = false;
-  };
-  networking.firewall.interfaces.ztzlgoe57z.allowedTCPPorts = [ 3389 ];
 
   hardware.opengl.driSupport32Bit = true;
   hardware.pulseaudio.support32Bit = true;
---
+++ nixos/work/xrdp/default.nix
@@ -0,0 +1,11 @@
+{ config, pkgs, inputs, ... }: {
+  services.xrdp = {
+    enable = true;
+    package = inputs.self.packages.x86_64-linux.custom-xrdp;
+  };
+  security.pam.services.xrdp-sesman = {
+    enableGnomeKeyring = true;
+    fprintAuth = false;
+  };
+  networking.firewall.interfaces.ztzlgoe57z.allowedTCPPorts = [ 3389 ];
+}
