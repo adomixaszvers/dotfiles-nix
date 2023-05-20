a02419eca566ba4489264f269d1d265709ce3767
Adomas Jatužis <adomixaszvers@gmail.com>
2023-05-20 12:02:39 +0300

    work: mount personal folder of storage serve

--- nixos/work/samba.nix
+++ nixos/work/samba.nix
@@ -1,13 +1,11 @@
 let
-  mkMount = rw: {
-    device = "//disk.insoft.dev/storage";
+  # this line prevents hanging on network split
+  automount_opts =
+    "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000,gid=100";
+  mkMount = device: rw: {
+    inherit device;
     fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts =
-        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,uid=1000,gid=100";
-
-    in [
+    options = [
       "${automount_opts},${
         if rw then "rw" else "ro"
       },credentials=/root/smb-secrets"
@@ -15,6 +13,7 @@ let
     noCheck = true;
   };
 in {
-  fileSystems."/kiti/insoft" = mkMount false;
-  fileSystems."/kiti/insoft_rw" = mkMount true;
+  fileSystems."/kiti/insoft" = mkMount "//disk.insoft.lt/storage" false;
+  fileSystems."/kiti/insoft_rw" = mkMount "//disk.insoft.lt/storage" true;
+  fileSystems."/kiti/home" = mkMount "//disk.insoft.lt/home" true;
 }
