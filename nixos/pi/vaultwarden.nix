3722ca300572aec1212f96d770948f1533fdfb15
Adomas Jatužis <adomixaszvers@gmail.com>
2023-05-20 13:41:57 +0300

    vaultwarden: use nginx as reverse proxy

--- nixos/pi/vaultwarden.nix
+++ nixos/pi/vaultwarden.nix
@@ -1,31 +1,27 @@
 {
-  services.traefik.dynamicConfigOptions.http = {
-    middlewares.vaultwarden-https-redirect.redirectScheme.scheme = "https";
-    routers =
-      let rule = "HostRegexp(`vaultwarden.{net:(lan|wg|zt)}.beastade.top`)";
-      in {
-        vaultwarden = {
-          inherit rule;
-          entrypoints = "web";
-          middlewares = "vaultwarden-https-redirect";
-          service = "vaultwarden";
-        };
-        vaultwarden-secure = {
-          inherit rule;
-          entrypoints = "websecure";
-          service = "vaultwarden";
-          tls = { };
-        };
-      };
-    services.vaultwarden.loadBalancer.servers =
-      [{ url = "http://127.0.0.1:8222"; }];
-  };
   services.vaultwarden = {
     enable = true;
     config = {
       signupsAllowed = true;
       rocketPort = 8222;
       rocketLog = "critical";
+    };
+  };
+  services.nginx.virtualHosts = let
+    locations = { "/" = { proxyPass = "http://127.0.0.1:8222"; }; };
+    forceSSL = true;
+  in {
+    "vaultwarden.lan.beastade.top" = {
+      useACMEHost = "lan.beastade.top";
+      inherit forceSSL locations;
+    };
+    "vaultwarden.wg.beastade.top" = {
+      useACMEHost = "wg.beastade.top";
+      inherit forceSSL locations;
+    };
+    "vaultwarden.zt.beastade.top" = {
+      useACMEHost = "zt.beastade.top";
+      inherit forceSSL locations;
     };
   };
 }
