cfb28503ad8c29dc758bd796966e6659970a6cc9
Adomas Jatužis <adomixaszvers@gmail.com>
2023-05-20 12:08:00 +0300

    use systemd tmpfiles for ld-link

--- nixos/ld-link.nix
+++ nixos/ld-link.nix
@@ -1,7 +1,5 @@
 { pkgs, lib, ... }: {
-  system.activationScripts.ldso = lib.stringAfter [ "usrbinenv" ] ''
-    mkdir -m 0755 -p /lib64
-    ln -sfn ${pkgs.glibc.out}/lib64/ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2.tmp
-    mv -f /lib64/ld-linux-x86-64.so.2.tmp /lib64/ld-linux-x86-64.so.2 # atomically replace
-  '';
+  systemd.tmpfiles.rules = [
+    "L+ /lib64/ld-linux-x86-64.so.2 - - - - ${pkgs.glibc}/lib64/ld-linux-x86-64.so.2"
+  ];
 }
