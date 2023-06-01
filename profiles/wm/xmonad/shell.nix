d5889cc76eed802638d0b2700eb0ee830d4be63d
Adomas Jatužis <adomixaszvers@gmail.com>
2023-06-01 14:05:15 +0300

    xmonad: fix nix shell

--- profiles/wm/xmonad/shell.nix
+++ profiles/wm/xmonad/shell.nix
@@ -1,18 +1,12 @@
 { pkgs ?
   (builtins.getFlake "nixpkgs").legacyPackages."${builtins.currentSystem}" }:
-let
-  extraPackages = import ./extraPackages.nix;
-  colors = import ./my-colors.nix;
-in with pkgs;
+with pkgs;
 mkShell {
   name = "xmonad-shell";
-  buildInputs = let
-    myHaskellPackages = pkgs.haskellPackages.override {
-      overrides = self: _super: { my-colors = self.callPackage colors { }; };
-    };
-  in [
-    (myHaskellPackages.ghcWithHoogle
-      (hs: (extraPackages hs) ++ [ hs.my-colors ]))
+  buildInputs = [
+    (haskellPackages.ghcWithHoogle (hs:
+      (import ./extraPackages.nix hs)
+      ++ [ (hs.callPackage ./my-colors.nix { }) ]))
     haskellPackages.haskell-language-server
   ];
 }
