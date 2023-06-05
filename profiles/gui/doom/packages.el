2805cd5e95597660040fd2841b25413b82b032bd
Adomas Jatužis <adomixaszvers@gmail.com>
2023-06-05 09:44:42 +0300

    doom emacs: remove redundant packages
    tree-sitter is enabled in init.el

--- profiles/gui/doom/packages.el
+++ profiles/gui/doom/packages.el
@@ -6,5 +6,3 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 (package! yuck-mode)
-(package! tree-sitter)
-(package! tree-sitter-langs)
