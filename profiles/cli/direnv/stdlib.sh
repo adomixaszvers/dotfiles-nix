9dbf4a3de72e43699a01b1f295d8678017cfd82d
Adomas Jatužis <adomixaszvers@gmail.com>
2023-06-19 04:17:17 +0300

    direnv: avoid spaces in direnv layouts

--- profiles/cli/direnv/stdlib.sh
+++ profiles/cli/direnv/stdlib.sh
@@ -1,10 +1,10 @@
 : "${XDG_CACHE_HOME:="${HOME}/.cache"}"
 declare -A direnv_layout_dirs
 direnv_layout_dir() {
-    local hash path
+    local dhash path
     echo "${direnv_layout_dirs[$PWD]:=$(
-        hash="$(sha1sum - <<< "$PWD")"
+        dhash="$(sha1sum - <<< "$PWD" | cut -d' ' -f1)"
         path="${PWD//[^a-zA-Z0-9]/-}"
-        echo "${XDG_CACHE_HOME}/direnv/layouts/${hash}${path}"
+        echo "${XDG_CACHE_HOME}/direnv/layouts/${dhash}${path}"
     )}"
 }
