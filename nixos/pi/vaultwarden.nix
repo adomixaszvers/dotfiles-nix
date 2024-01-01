8be278dfa46a9b44fd0da9ab9ea05b91f9edf9d5
Adomas Jatužis <adomixaszvers@gmail.com>
2024-01-02 00:47:58 +0200

    vaultwarden: try Yubico OTP

--- nixos/pi/vaultwarden.nix
+++ nixos/pi/vaultwarden.nix
@@ -1,4 +1,9 @@
-{
+{ config, ... }: {
+  sops.secrets."vaultwarden.env" = {
+    sopsFile = ./secrets/vaulwarden.env;
+    owrner = config.users.users.vaultwarden.name;
+    inherit (config.users.users.vaultwarden) group;
+  };
   services.vaultwarden = {
     enable = true;
     config = {
@@ -6,6 +11,7 @@
       rocketPort = 8222;
       rocketLog = "critical";
     };
+    environmentFile = config.sops.secrets."vaultwarden.env".path;
   };
   services.nginx.virtualHosts = let
     locations = { "/" = { proxyPass = "http://127.0.0.1:8222"; }; };
