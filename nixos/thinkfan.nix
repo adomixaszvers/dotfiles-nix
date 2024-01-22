d75d4a95d9f0bfeea0b9a8dd696e5c128d1d67dc
Adomas Jatužis <adomixaszvers@gmail.com>
2024-01-22 11:57:52 +0200

    work: change thinkfan config

--- nixos/thinkfan.nix
+++ nixos/thinkfan.nix
@@ -1,7 +1,4 @@
 { lib, ... }: {
-  boot.extraModprobeConfig = ''
-    options thinkpad_acpi fan_control=1
-  '';
   services.thinkfan = {
     enable = lib.mkDefault true;
     levels = [ [ 0 0 61 ] [ "level auto" 60 32767 ] ];
--- nixos/work/configuration.nix
+++ nixos/work/configuration.nix
@@ -134,6 +134,15 @@
       ports = [ 9222 22 ];
     };
     printing.enable = true;
+    thinkfan.levels = [
+      [ 0 0 56 ]
+      [ 1 55 66 ]
+      [ 2 65 71 ]
+      [ 3 70 76 ]
+      [ 4 75 81 ]
+      [ 5 80 86 ]
+      [ 7 85 32767 ]
+    ];
     vnstat.enable = true;
   };
 
