c7b335cd8c6246d3a5e37bb84dc800c91a4c7bd7
Adomas Jatužis <adomixaszvers@gmail.com>
2024-03-22 13:44:04 +0200

    expire home-manager generations
    see https://git.sr.ht/~r-vdp/nixos-config/tree/c4335ada77879f254a5a58850edf5e9bbd0f7d77/item/home-modules/default.nix#L164

--- profiles/common.nix
+++ profiles/common.nix
@@ -1,11 +1,39 @@
-{ inputs, ... }: {
+{ inputs, pkgs, lib, config, ... }: {
   imports = [ ./cli ./gui ./wm/common.nix ];
   manual.html.enable = true;
+  nix.package = lib.mkDefault pkgs.nix;
   programs.home-manager = {
     enable = true;
     path = inputs.home-manager.outPath;
   };
-  systemd.user = { startServices = "sd-switch"; };
+  systemd.user = {
+    startServices = "sd-switch";
+    services.expire-hm-profiles = {
+      Unit.Description = "Clean up HM user profiles.";
 
-  xdg = { enable = true; };
+      Service = {
+        Type = "oneshot";
+        ExecStart = lib.getExe (pkgs.writeShellApplication {
+          name = "expire-hm-profiles-start-script";
+          text = ''
+            ${lib.getExe config.nix.package} profile wipe-history \
+              --profile "${config.xdg.stateHome}/nix/profiles/home-manager" \
+              --older-than '14d'
+          '';
+        });
+      };
+    };
+    timers.expire-hm-profiles = {
+      Unit.Description = "Clean up HM user profiles.";
+
+      Timer = {
+        OnCalendar = "weekly";
+        Persistent = true;
+      };
+
+      Install.WantedBy = [ "timers.target" ];
+    };
+  };
+
+  xdg.enable = true;
 }
