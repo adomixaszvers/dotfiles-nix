f8942fa1d6548905f3cc6b2529611aba87ee114d
Adomas Jatužis <adomixaszvers@gmail.com>
2024-03-24 23:44:02 +0200

    neovim: isolate where lua lsp is added

--- profiles/cli/neovim/default.nix
+++ profiles/cli/neovim/default.nix
@@ -1,9 +1,9 @@
 { pkgs, ... }: {
-  home.packages = with pkgs; [ lua-language-server ];
   programs.neovim = {
     enable = true;
     package = pkgs.neovim-unwrapped;
     plugins = import ./plugins.nix pkgs;
     extraConfig = import ./customRc.nix;
+    extraPackages = with pkgs; [ lua-language-server ];
   };
 }
