e57311ba02e3fecae49aba45109a2a2161ade9c0
Adomas Jatužis <adomixaszvers@gmail.com>
2024-06-01 10:21:53 +0300

    work: switch to performance profile on AC

--- nixos/work/tlp.nix
+++ nixos/work/tlp.nix
@@ -2,7 +2,7 @@
   services.tlp = {
     enable = true;
     settings = {
-      PLATFORM_PROFILE_ON_AC = "balanced";
+      PLATFORM_PROFILE_ON_AC = "performance";
       PLATFORM_PROFILE_ON_BAT = "low-power";
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
