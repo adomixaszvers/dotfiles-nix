ff13284329a1175e21be9604e1593ce302710fbf
Adomas Jatužis <adomixaszvers@gmail.com>
2024-06-01 10:21:03 +0300

    xrdp: use ~/startwm.sh

--- nixos/work/xrdp/current.nix
+++ nixos/work/xrdp/current.nix
@@ -4,14 +4,28 @@
   };
   services.xrdp = {
     enable = true;
-    extraConfDirCommands = let inherit (inputs) self;
+    extraConfDirCommands = let
+      inherit (inputs) self;
+      cfg = config.services.xrdp;
+      # bash
     in ''
       cp ${
         builtins.path { path = "${self}/pkgs/custom-xrdp/km-00010427.ini"; }
-      } $out
+      } $out/km-00010427.ini
       cp ${
         builtins.path { path = "${self}/pkgs/custom-xrdp/xrdp_keyboard.ini"; }
-      } $out
+      } $out/xrdp_keyboard.ini
+
+      cat > $out/startwm.sh <<EOF
+      #!/bin/sh
+      . /etc/profile
+      if [ -f ~/startwm.sh ] && [ -x ~/startwm.sh ]; then
+        ~/startwm.sh
+      else
+        ${cfg.defaultWindowManager}
+      fi
+      EOF
+      chmod +x $out/startwm.sh
     '';
   };
   security.pam.services.xrdp-sesman = {
