6ceadd4b1dfefb5d165da6f9eeabfcce837c13c8
Adomas Jatužis <adomixaszvers@gmail.com>
2024-06-03 09:29:19 +0300

    work: win10 does not work with OVMFFull :/

--- nixos/libvirtd.nix
+++ nixos/libvirtd.nix
@@ -1,12 +1,8 @@
-{ pkgs, ... }: {
+{
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
       runAsRoot = false;
-      ovmf = {
-        enable = true;
-        packages = [ pkgs.OVMFFull.fd ];
-      };
       swtpm.enable = true;
     };
   };
