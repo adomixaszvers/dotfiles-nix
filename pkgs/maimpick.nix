41f8140ccc6273dd812eb0a1361bcc359b47d18b
Adomas Jatužis <adomixaszvers@gmail.com>
2024-07-26 12:42:50 +0300

    maimpick: copy changes from voidrice

--- pkgs/maimpick.nix
+++ pkgs/maimpick.nix
@@ -1,12 +1,15 @@
 { writeShellScriptBin, lib, coreutils, maim, xdotool, xclip }:
-writeShellScriptBin "maimpick" ''
+let
+  output = "$(date '+%y%m%d-%H%M-%S').png";
+  xclip_cmd = "xclip -sel clip -t image/png";
+in writeShellScriptBin "maimpick" ''
   PATH=$PATH:${lib.makeBinPath [ coreutils maim xdotool xclip ]}
   case "$(printf "a selected area\\ncurrent window\\nfull screen\\na selected area (copy)\\ncurrent window (copy)\\nfull screen (copy)" | rofi -dmenu -l 6 -i -p "Screenshot which area?")" in
-    "a selected area") maim -s pic-selected-"$(date '+%y%m%d-%H%M-%S').png" ;;
-    "current window") maim -i "$(xdotool getactivewindow)" pic-window-"$(date '+%y%m%d-%H%M-%S').png" ;;
-    "full screen") maim pic-full-"$(date '+%y%m%d-%H%M-%S').png" ;;
-    "a selected area (copy)") maim -s | xclip -selection clipboard -t image/png ;;
-    "current window (copy)") maim -i "$(xdotool getactivewindow)" | xclip -selection clipboard -t image/png ;;
-    "full screen (copy)") maim | xclip -selection clipboard -t image/png ;;
+    "a selected area") maim -u -s pic-selected-"${output}" ;;
+    "current window") maim -B -q -d 0.2 -i "$(xdotool getactivewindow)" pic-window-"${output}" ;;
+    "full screen") maim -q -d 0.2 pic-full-"${output}" ;;
+    "a selected area (copy)") maim -u -s | ${xclip_cmd} ;;
+    "current window (copy)") maim -q -d 0.2 -i "$(xdotool getactivewindow)" | ${xclip_cmd} ;;
+    "full screen (copy)") maim -q -d 0.2 | ${xclip_cmd} ;;
   esac
 ''
