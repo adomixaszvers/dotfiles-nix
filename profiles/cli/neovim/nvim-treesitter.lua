9ee64abae8746905ebf9d0f2777fdc4d920b7009
Adomas Jatužis <adomixaszvers@gmail.com>
2024-12-20 10:00:32 +0200

    neovim: disable tree-sitter highlighting in large files

--- profiles/cli/neovim/nvim-treesitter.lua
+++ profiles/cli/neovim/nvim-treesitter.lua
@@ -1,15 +1,23 @@
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
     highlight = {
-        enable = true,              -- false will disable the whole extension
+        enable = true, -- false will disable the whole extension
         -- disable = { "bash" },
+        is_supported = function()
+            if vim.fn.strwidth(vim.fn.getline('.')) > 300
+                or vim.fn.getfsize(vim.fn.expand('%')) > 1024 * 1024 then
+                return false
+            else
+                return true
+            end
+        end,
     },
     incremental_selection = {
         enable = true,
         keymaps = {
-        init_selection = "<leader>gn",
-        node_incremental = "grn",
-        scope_incremental = "grc",
-        node_decremental = "grm",
+            init_selection = "<leader>gn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
         },
     },
 }
