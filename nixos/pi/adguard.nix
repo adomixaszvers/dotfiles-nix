2690ddb03e67e5819fdc0413ef7cd130882ec48b
Adomas Jatužis <adomixaszvers@gmail.com>
2025-01-14 16:11:21 +0200

    adguard: use DNS over HTTPS upstream

--- nixos/pi/adguard.nix
+++ nixos/pi/adguard.nix
@@ -43,9 +43,13 @@
         ];
         ratelimit = 0;
         upstream_dns = [
+          "https://cloudflare-dns.com/dns-query"
+          # "https://dns.quad9.net/dns-query"
+          "[/lan/]192.168.1.254"
+        ];
+        bootstrap_dns = [
           "9.9.9.9"
           "149.112.112.112"
-          "[/lan/]192.168.1.254"
         ];
         aaaa_disabled = true;
         local_ptr_upstreams = [ "192.168.1.254" ];
