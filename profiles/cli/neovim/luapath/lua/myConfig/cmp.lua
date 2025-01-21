211b9c61e4ce78e2dc611ec4e80d44394cb5b4f8
Adomas Jatužis <adomixaszvers@gmail.com>
2025-01-21 17:54:13 +0200

    neovim: complete from all visible buffers

--- profiles/cli/neovim/luapath/lua/myConfig/cmp.lua
+++ profiles/cli/neovim/luapath/lua/myConfig/cmp.lua
@@ -30,7 +30,19 @@ cmp.setup({
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
     }, {
-        { name = 'buffer' },
+        {
+            name = 'buffer',
+            option = {
+                -- completes from visible buffers
+                get_bufnrs = function()
+                    local bufs = {}
+                    for _, win in ipairs(vim.api.nvim_list_wins()) do
+                        bufs[vim.api.nvim_win_get_buf(win)] = true
+                    end
+                    return vim.tbl_keys(bufs)
+                end
+            },
+        },
     })
 })
 
