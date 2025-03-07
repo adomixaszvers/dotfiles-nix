15a6b071c0fbc791766bca5302422828671482ae
Adomas Jatužis <adomixaszvers@gmail.com>
2025-03-07 10:41:47 +0200

    work: fix vpnc tab in nm applet

--- nixos/work/configuration.nix
+++ nixos/work/configuration.nix
@@ -61,6 +61,16 @@
     '';
   };
 
+  nixpkgs.overlays = [
+    (_: super: {
+      networkmanager-vpnc = super.networkmanager-vpnc.overrideAttrs (old: {
+        patches = old.patches ++ [
+          ../../export_nm_vpn_editor_factory_vpnc.patch
+        ];
+      });
+    })
+  ];
+
   networking = {
     hostId = "d864861a";
     hostName = "adomas-jatuzis-nixos"; # Define your hostname.
