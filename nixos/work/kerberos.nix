7bf020ffa4fced01486a16e5406377860de8ceac
Adomas Jatužis <adomixaszvers@gmail.com>
2025-03-19 15:12:33 +0200

    kerberos: use persistent keyring ccache
    see https://web.mit.edu/kerberos/krb5-latest/doc/basic/ccache_def.html?highlight=keyring#ccache-types

--- nixos/work/kerberos.nix
+++ nixos/work/kerberos.nix
@@ -4,7 +4,7 @@
     settings = {
       libdefaults = {
         default_realm = "X.INSOFT.LT";
-        default_ccache_name = "/home/%{username}/.cache/krb5cc";
+        default_ccache_name = "KEYRING:persistent:%{uid}";
       };
       realms."X.INSOFT.LT".pkinit_anchors = "FILE:${./insoft-ca.crt}";
     };
