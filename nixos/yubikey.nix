bea74cd91fe7c897a8487acbe57564caa123ab9c
Adomas Jatužis <adomixaszvers@gmail.com>
2025-05-23 16:22:48 +0300

    yubikey: use PIV for SSH authentication

--- nixos/yubikey.nix
+++ nixos/yubikey.nix
@@ -5,6 +5,9 @@
     yubikey-manager-qt
   ];
   hardware.gpgSmartcards.enable = true;
+  programs.ssh.extraConfig = ''
+    PKCS11Provider "${pkgs.yubico-piv-tool}/lib/libykcs11.so"
+  '';
   services = {
     udev.packages = with pkgs; [
       yubikey-personalization
