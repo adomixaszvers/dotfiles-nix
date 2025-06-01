c1c79f2377fa254440e0674eebe0315e8c0a9c2d
Adomas Jatužis <adomixaszvers@gmail.com>
2025-06-01 13:53:50 +0300

    neovim: disable ale if lsp is enabled

--- profiles/cli/neovim/luapath/lua/myConfig/lspconfig.lua
+++ profiles/cli/neovim/luapath/lua/myConfig/lspconfig.lua
@@ -3,8 +3,9 @@ local nvim_lsp = require('lspconfig')
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+  vim.cmd('ALEDisableBuffer')
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+  local function buf_set_option(name, value) vim.api.nvim_set_option_value(name, value, { buf = bufnr}) end
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
