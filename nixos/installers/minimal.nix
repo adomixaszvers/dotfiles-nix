91fdab85dd54ae52797673adfedf5bb7d7f8f11e
Adomas Jatužis <adomixaszvers@gmail.com>
2025-06-24 20:34:21 +0300

    use modulesPath in installer derivations

--- nixos/installers/minimal.nix
+++ nixos/installers/minimal.nix
@@ -1,7 +1,7 @@
-{ inputs, ... }:
+{ modulesPath, ... }:
 {
   imports = [
-    "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+    (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix")
     ./common.nix
   ];
 }
--- nixos/installers/plasma6.nix
+++ nixos/installers/plasma6.nix
@@ -1,7 +1,7 @@
-{ inputs, ... }:
+{ modulesPath, ... }:
 {
   imports = [
-    "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix"
+    (modulesPath + "/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix")
     ./common.nix
   ];
 }
