f7ebd32ca9512ceacd3ea5b4d55e3766409dd512
Adomas Jatužis <adomixaszvers@gmail.com>
2025-07-06 10:25:10 +0300

    nextcloud: add image previews

--- nixos/pi/nextcloud.nix
+++ nixos/pi/nextcloud.nix
@@ -30,6 +30,19 @@
           hostname = "localhost";
         };
       };
+      settings.enabledPreviewProviders = [
+        "OC\\Preview\\BMP"
+        "OC\\Preview\\GIF"
+        "OC\\Preview\\JPEG"
+        "OC\\Preview\\Krita"
+        "OC\\Preview\\MarkDown"
+        "OC\\Preview\\MP3"
+        "OC\\Preview\\OpenDocument"
+        "OC\\Preview\\PNG"
+        "OC\\Preview\\TXT"
+        "OC\\Preview\\XBitmap"
+        "OC\\Preview\\HEIC"
+      ];
       hostName = "nextcloud.lan.beastade.top";
       https = true;
       configureRedis = true;
