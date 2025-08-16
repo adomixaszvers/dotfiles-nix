d707a4df3310160b66f2b362875591ecb6e78226
Adomas Jatužis <adomixaszvers@gmail.com>
2025-08-16 18:36:54 +0300

    firefox: declaratively add search engines

--- profiles/gui/firefox.nix
+++ profiles/gui/firefox.nix
@@ -5,6 +5,45 @@
       DisablePocket = true;
       DisableTelemetry = true;
       PasswordManagerEnabled = false;
+      Preferences = {
+        "browser.urlbar.suggest.remotetab" = {
+          Value = false;
+          Status = "user";
+        };
+      };
+      SearchEngines = {
+        Add = [
+          {
+            Name = "NixOS Package Search";
+            Alias = "nixp";
+            URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
+            Method = "GET"; # "POST"
+            IconURL = "https://search.nixos.org/favicon.png";
+          }
+          {
+            Name = "NixOS Option Search";
+            Alias = "nixo";
+            URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
+            Method = "GET"; # "POST"
+            IconURL = "https://search.nixos.org/favicon.png";
+          }
+          {
+            Name = "Searx English";
+            Alias = "sx";
+            URLTemplate = "https://searx.lan.beastade.top/search?q={searchTerms}&language=en&&safesearch=2&";
+            Method = "GET";
+            IconURL = "https://searx.lan.beastade.top/favicon.ico";
+          }
+          {
+            Name = "Searx Lithuanian";
+            Alias = "slt";
+            URLTemplate = "https://searx.lan.beastade.top/search?q={searchTerms}&language=lt&&safesearch=2&";
+            Method = "GET";
+            IconURL = "https://searx.lan.beastade.top/favicon.ico";
+          }
+        ];
+        Default = "Searx English";
+      };
     };
   };
 }
