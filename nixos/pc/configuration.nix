2ae6ac7104da02ed651dcf83dd9126e84a37a954
Adomas Jatužis <adomixaszvers@gmail.com>
2025-08-23 11:21:36 +0300

    pc: use nvidia 580 drivers

--- nixos/pc/configuration.nix
+++ nixos/pc/configuration.nix
@@ -71,6 +71,14 @@
       enable32Bit = true;
     };
     nvidia = {
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+        version = "580.76.05";
+        sha256_64bit = "sha256-IZvmNrYJMbAhsujB4O/4hzY8cx+KlAyqh7zAVNBdl/0=";
+        sha256_aarch64 = "sha256-NL2DswzVWQQMVM092NmfImqKbTk9VRgLL8xf4QEvGAQ=";
+        openSha256 = "sha256-xEPJ9nskN1kISnSbfBigVaO6Mw03wyHebqQOQmUg/eQ=";
+        settingsSha256 = "sha256-ll7HD7dVPHKUyp5+zvLeNqAb6hCpxfwuSyi+SAXapoQ=";
+        persistencedSha256 = "sha256-bs3bUi8LgBu05uTzpn2ugcNYgR5rzWEPaTlgm0TIpHY=";
+      };
       open = true;
       prime = {
         offload.enable = false;
