edaf7a10a1c72bbf77549f520574587fd4262e18
Adomas Jatužis <adomixaszvers@gmail.com>
2025-08-29 00:12:29 +0300

    change prebuild scripts

--- nixos/work/prebuild-configs.nix
+++ nixos/work/prebuild-configs.nix
@@ -2,16 +2,19 @@
 {
   systemd = {
     services.prebuild-configs = {
+      serviceConfig.User = "adomas";
       description = "Prebuild personal configs";
       script =
         let
           nix = config.nix.package;
-          flakeRef = "github:adomixaszvers/dotfiles-nix/update_flake_lock_action";
+          flakeRef = "git+file:///home/adomas/.config/nixpkgs";
+          referenced = "github:adomixaszvers/dotfiles-nix/update_flake_lock_action";
         in
+        # bash
         ''
           PATH=${lib.makeBinPath [ nix ]}:$PATH
-          nix flake archive --refresh '${flakeRef}'
-          nix build --no-link '${flakeRef}#nixosConfigurations.adomas-jatuzis-nixos.config.system.build.toplevel' '${flakeRef}#homeConfigurations.work.activationPackage' '${flakeRef}#homeConfigurations.work-remote.activationPackage' '${flakeRef}#devShells.x86_64-linux.default'
+          nix flake archive --refresh '${referenced}'
+          nix build --no-link --no-write-lock-file --reference-lock-file '${referenced}' '${flakeRef}#nixosConfigurations.adomas-jatuzis-nixos.config.system.build.toplevel' '${flakeRef}#homeConfigurations.work.activationPackage' '${flakeRef}#homeConfigurations.work-remote.activationPackage' '${flakeRef}#devShells.x86_64-linux.default'
         '';
       startAt = "Fri, 06:00";
     };
