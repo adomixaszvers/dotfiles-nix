ba4f03f9253878505fc32c43417114f6ea2a6abe
Adomas Jatužis <adomixaszvers@gmail.com>
2025-10-27 16:43:04 +0200

    disable beesd

--- nixos/work/btrfs.nix
+++ nixos/work/btrfs.nix
@@ -41,16 +41,16 @@ in
     enable = true;
     fileSystems = [ "/" ];
   };
-  services.beesd.filesystems = {
-    rpool = {
-      spec = "LABEL=rpool";
-      hashTableSizeMB = 512;
-      verbosity = "crit";
-      extraOptions = [
-        "--loadavg-target"
-        "5.0"
-      ];
-    };
-  };
+  # services.beesd.filesystems = {
+  #   rpool = {
+  #     spec = "LABEL=rpool";
+  #     hashTableSizeMB = 512;
+  #     verbosity = "crit";
+  #     extraOptions = [
+  #       "--loadavg-target"
+  #       "5.0"
+  #     ];
+  #   };
+  # };
 
 }
