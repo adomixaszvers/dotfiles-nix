bb0787033309d42ac2a7bdf5f5066f7e90091d70
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-04 16:36:13 +0200

    disable ssh kitten
    see https://github.com/kovidgoyal/kitty/issues/9078
    it should work again with openssh 10.2p1

--- profiles/gui/kitty.nix
+++ profiles/gui/kitty.nix
@@ -11,12 +11,13 @@
 
     };
   };
-  programs.zsh.initContent = # bash
-    ''
-      if [ "$TERM" = 'xterm-kitty' ] && [ -x "$(command -v kitty)" ] && [ -n "$DISPLAY" ]; then
-        alias ssh='kitty +kitten ssh'
-      fi
-    '';
+  # does not work with openssh 10.1
+  # programs.zsh.initContent = # bash
+  #   ''
+  #     if [ "$TERM" = 'xterm-kitty' ] && [ -x "$(command -v kitty)" ] && [ -n "$DISPLAY" ]; then
+  #       alias ssh='kitty +kitten ssh'
+  #     fi
+  #   '';
   stylix.targets.kitty = {
     enable = true;
     # workaround for bright green looking like black color
