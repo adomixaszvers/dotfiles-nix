e003b8a85a82d6bffcf90fb40665104556071f6d
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-07 18:39:33 +0200

    Revert "drop chaotic-cx/nyx"
    This reverts commit bbfa3480d8347ab736f9662821f24e1713380d5b.

--- nixos/pc/configuration.nix
+++ nixos/pc/configuration.nix
@@ -11,6 +11,7 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
     ../common.nix
+    ../cachyos.nix
     ../flakes.nix
     ../gc.nix
     ../nix-registry.nix
@@ -71,14 +72,6 @@
       enable32Bit = true;
     };
     nvidia = {
-      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "580.95.05";
-        sha256_64bit = "sha256-hJ7w746EK5gGss3p8RwTA9VPGpp2lGfk5dlhsv4Rgqc=";
-        sha256_aarch64 = "sha256-zLRCbpiik2fGDa+d80wqV3ZV1U1b4lRjzNQJsLLlICk=";
-        openSha256 = "sha256-RFwDGQOi9jVngVONCOB5m/IYKZIeGEle7h0+0yGnBEI=";
-        settingsSha256 = "sha256-F2wmUEaRrpR1Vz0TQSwVK4Fv13f3J9NJLtBe4UP2f14=";
-        persistencedSha256 = "sha256-QCwxXQfG/Pa7jSTBB0xD3lsIofcerAWWAHKvWjWGQtg=";
-      };
       open = true;
       prime = {
         offload.enable = false;
