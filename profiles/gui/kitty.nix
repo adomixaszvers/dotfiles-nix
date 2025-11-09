84ac86ff34583ba12863227e37bfce29b9669986
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-09 15:39:10 +0200

    Revert "disable ssh kitten"
    This reverts commit bb0787033309d42ac2a7bdf5f5066f7e90091d70.

--- profiles/gui/kitty.nix
+++ profiles/gui/kitty.nix
@@ -11,13 +11,12 @@
 
     };
   };
-  # does not work with openssh 10.1
-  # programs.zsh.initContent = # bash
-  #   ''
-  #     if [ "$TERM" = 'xterm-kitty' ] && [ -x "$(command -v kitty)" ] && [ -n "$DISPLAY" ]; then
-  #       alias ssh='kitty +kitten ssh'
-  #     fi
-  #   '';
+  programs.zsh.initContent = # bash
+    ''
+      if [ "$TERM" = 'xterm-kitty' ] && [ -x "$(command -v kitty)" ] && [ -n "$DISPLAY" ]; then
+        alias ssh='kitty +kitten ssh'
+      fi
+    '';
   stylix.targets.kitty = {
     enable = true;
     # workaround for bright green looking like black color
