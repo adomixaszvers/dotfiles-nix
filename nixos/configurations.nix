e45b4e3e0a7602d7d5ea593dc5288535764df0c0
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-09 15:23:31 +0200

    workaround for ssh multiplexing issues

--- nixos/configurations.nix
+++ nixos/configurations.nix
@@ -9,23 +9,38 @@ in
   flake.nixosConfigurations = {
     adomo-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
-      modules = [ ./home/configuration.nix ];
+      modules = [
+        ./home/configuration.nix
+        ./ssh-workaround.nix
+      ];
     };
     adomas-jatuzis-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
-      modules = [ ./work/configuration.nix ];
+      modules = [
+        ./work/configuration.nix
+        ./ssh-workaround.nix
+      ];
     };
     adomo-pc-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
-      modules = [ ./pc/configuration.nix ];
+      modules = [
+        ./pc/configuration.nix
+        ./ssh-workaround.nix
+      ];
     };
     adomo-t14 = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
-      modules = [ ./t14/configuration.nix ];
+      modules = [
+        ./t14/configuration.nix
+        ./ssh-workaround.nix
+      ];
     };
     raspberrypi-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
-      modules = [ ./pi/configuration.nix ];
+      modules = [
+        ./pi/configuration.nix
+        ./ssh-workaround.nix
+      ];
     };
 
   };
