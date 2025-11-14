3ce51a9a34b72b94919bfda07ecbf27accaa7dd8
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-14 17:07:20 +0200

    Revert "change prebuild scripts"
    This reverts commit edaf7a10a1c72bbf77549f520574587fd4262e18.

--- nixos/pi/prebuild-configs.nix
+++ nixos/pi/prebuild-configs.nix
@@ -7,14 +7,13 @@
       script =
         let
           nix = config.nix.package;
-          flakeRef = "git+file:///home/pi/.config/nixpkgs";
-          referenced = "github:adomixaszvers/dotfiles-nix/update_flake_lock_action";
+          flakeRef = "github:adomixaszvers/dotfiles-nix/update_flake_lock_action";
         in
         # bash
         ''
           PATH=${lib.makeBinPath [ nix ]}:$PATH
-          nix flake archive --refresh '${referenced}'
-          nix build --no-link --no-write-lock-file --reference-lock-file '${referenced}' '${flakeRef}#nixosConfigurations.raspberrypi-nixos.config.system.build.toplevel' '${flakeRef}#homeConfigurations.pi.activationPackage' '${flakeRef}#devShells.aarch64-linux.default'
+          nix flake archive --refresh '${flakeRef}'
+          nix build --no-link '${flakeRef}#nixosConfigurations.raspberrypi-nixos.config.system.build.toplevel' '${flakeRef}#homeConfigurations.pi.activationPackage' '${flakeRef}#devShells.aarch64-linux.default'
         '';
       startAt = "Fri, 06:00";
     };
