b776d1ec35e60247ec09d7bf98e49e87b46a1a5e
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-14 07:44:05 +0200

    ssh: workaround is no longer needed

--- nixos/configurations.nix
+++ nixos/configurations.nix
@@ -11,35 +11,30 @@ in
       inherit specialArgs;
       modules = [
         ./home/configuration.nix
-        ./ssh-workaround.nix
       ];
     };
     adomas-jatuzis-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
         ./work/configuration.nix
-        ./ssh-workaround.nix
       ];
     };
     adomo-pc-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
         ./pc/configuration.nix
-        ./ssh-workaround.nix
       ];
     };
     adomo-t14 = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
         ./t14/configuration.nix
-        ./ssh-workaround.nix
       ];
     };
     raspberrypi-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
         ./pi/configuration.nix
-        ./ssh-workaround.nix
       ];
     };
 
