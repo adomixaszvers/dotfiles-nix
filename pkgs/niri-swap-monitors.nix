103f25cc2098e9b29bb31288bbab2c0391fff5a0
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-16 23:41:44 +0200

    niri: update config

--- pkgs/niri-swap-monitors.nix
+++ pkgs/niri-swap-monitors.nix
@@ -1,6 +1,12 @@
-{ writers }:
+{
+  writers,
+  jq,
+  lib,
+}:
 writers.writeDashBin "niri-swap-monitors" ''
-  set -e
+  if [ "$(niri msg -j outputs | ${lib.getExe jq} length)" = 1 ]; then
+    exit 0
+  fi
   niri msg action do-screen-transition --delay-ms 150
   niri msg action move-workspace-to-monitor-next
   niri msg action focus-workspace-previous
