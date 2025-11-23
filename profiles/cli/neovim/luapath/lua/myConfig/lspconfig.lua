433c63ba2986aeb480c0bcca884a4b354129e255
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-23 17:04:07 +0200

    neovim: remove unused import

--- profiles/cli/neovim/luapath/lua/myConfig/lspconfig.lua
+++ profiles/cli/neovim/luapath/lua/myConfig/lspconfig.lua
@@ -1,5 +1,3 @@
-local nvim_lsp = require('lspconfig')
-
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
