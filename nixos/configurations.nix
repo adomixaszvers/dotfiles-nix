33a9c1508d9db8497c0749ffe2be9c1cadabb362
Adomas Jatužis <adomixaszvers@gmail.com>
2025-11-29 10:06:59 +0200

    try determinate nix again

--- nixos/configurations.nix
+++ nixos/configurations.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 let
-  inherit (inputs) nixpkgs;
+  inherit (inputs) nixpkgs determinate;
   specialArgs = {
     inherit inputs;
   };
@@ -10,30 +10,35 @@ in
     adomo-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
+        determinate.nixosModules.default
         ./home/configuration.nix
       ];
     };
     adomas-jatuzis-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
+        determinate.nixosModules.default
         ./work/configuration.nix
       ];
     };
     adomo-pc-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
+        determinate.nixosModules.default
         ./pc/configuration.nix
       ];
     };
     adomo-t14 = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
+        determinate.nixosModules.default
         ./t14/configuration.nix
       ];
     };
     raspberrypi-nixos = nixpkgs.lib.nixosSystem {
       inherit specialArgs;
       modules = [
+        determinate.nixosModules.default
         ./pi/configuration.nix
       ];
     };
