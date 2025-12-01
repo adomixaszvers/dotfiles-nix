885e15f695ba2bf6e369b074d6d00f059e60f5f7
Adomas Jatužis <adomixaszvers@gmail.com>
2025-12-01 17:36:58 +0200

    syncthing: use module option to open ports

--- nixos/syncthing.nix
+++ nixos/syncthing.nix
@@ -2,16 +2,9 @@
   # see https://github.com/lucas-clemente/quic-go/wiki/UDP-Receive-Buffer-Size#non-bsd
   boot.kernel.sysctl."net.core.rmem_max" = 2500000;
   # Syncthing ports
-  networking.firewall.allowedTCPPorts = [
-    # 8384
-    22000
-  ];
-  networking.firewall.allowedUDPPorts = [
-    22000
-    21027
-  ];
   services.syncthing = {
     enable = true;
+    openDefaultPorts = true;
     user = "adomas";
     group = "users";
     dataDir = "/home/adomas";
