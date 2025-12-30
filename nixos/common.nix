ee00bb490bc55e3c8336e21dddc39e0452807481
Adomas Jatužis <adomixaszvers@gmail.com>
2025-12-30 14:04:36 +0200

    add nixos-cuda cache

--- nixos/common.nix
+++ nixos/common.nix
@@ -8,11 +8,13 @@
       substituters = lib.mkAfter [
         "https://pre-commit-hooks.cachix.org"
         "https://nix-community.cachix.org"
+        "https://cache.nixos-cuda.org"
         "https://adomixaszvers.cachix.org"
       ];
       trusted-public-keys = [
         "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "adomixaszvers.cachix.org-1:r3/lrlbDE7o/Vjk/muEU2iLIiCEZMbC09ZqiwAs64so="
       ];
       auto-optimise-store = true;
