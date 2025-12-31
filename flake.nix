5e14611df44a11597e5628ee3ee27f853ea3c4cd
Adomas Jatužis <adomixaszvers@gmail.com>
2025-12-31 08:26:08 +0200

    nixCats: bump to 7.3.3

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
         xwayland-satellite-unstable.follows = "";
       };
     };
-    nixCats.url = "github:BirdeeHub/nixCats-nvim/v7.3.2";
+    nixCats.url = "github:BirdeeHub/nixCats-nvim/v7.3.3";
     nixGL = {
       url = "github:nix-community/nixGL";
       inputs = {
