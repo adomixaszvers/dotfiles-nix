aa18c6e6e4cdcefb20a19678a4a9f18ff658a0ef
Adomas Jatužis <adomixaszvers@gmail.com>
2026-01-02 15:49:39 +0200

    prebuild-configs: use `inputs-from`

--- nixos/work/prebuild-configs.nix
+++ nixos/work/prebuild-configs.nix
@@ -7,13 +7,14 @@
       script =
         let
           nix = config.nix.package;
-          flakeRef = "github:adomixaszvers/dotfiles-nix/update_flake_lock_action";
+          flakeRef = "git+file:///home/adomas/.config/nixpkgs";
+          updatesFlakeRef = "github:adomixaszvers/dotfiles-nix/update_flake_lock_action";
         in
         # bash
         ''
           PATH=${lib.makeBinPath [ nix ]}:$PATH
-          nix flake archive --refresh '${flakeRef}'
-          nix build --no-link --keep-going '${flakeRef}#nixosConfigurations.adomas-jatuzis-nixos.config.system.build.toplevel' '${flakeRef}#homeConfigurations.work.activationPackage' '${flakeRef}#homeConfigurations.work-remote.activationPackage' '${flakeRef}#devShells.x86_64-linux.default'
+          nix flake archive --refresh '${updatesFlakeRef}'
+          nix build --no-link --keep-going --inputs-from '${updatesFlakeRef}' '${flakeRef}#nixosConfigurations.adomas-jatuzis-nixos.config.system.build.toplevel' '${flakeRef}#homeConfigurations.work.activationPackage' '${flakeRef}#homeConfigurations.work-remote.activationPackage' '${flakeRef}#devShells.x86_64-linux.default'
         '';
       startAt = "Fri, 06:00";
     };
