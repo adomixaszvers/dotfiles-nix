2ce6b70ae2e3c9252d6c697b35dcefd8a8f9108a
Adomas Jatužis <adomixaszvers@gmail.com>
2026-01-03 19:39:02 +0200

    refactor prebuild-configs

--- nixos/pi/prebuild-configs.nix
+++ nixos/pi/prebuild-configs.nix
@@ -1,20 +1,18 @@
-{ config, lib, ... }:
+{ config, ... }:
 {
   systemd = {
     services.prebuild-configs = {
+      path = [ config.nix.package ];
       serviceConfig.User = "pi";
       description = "Prebuild personal configs";
       script =
         let
-          nix = config.nix.package;
-          flakeRef = "git+file:///home/pi/.config/nixpkgs";
-          updatesFlakeRef = "github:adomixaszvers/dotfiles-nix/update_flake_lock_action";
+          flakeRef = "github:adomixaszvers/dotfiles-nix/update_flake_lock_action";
         in
         # bash
         ''
-          PATH=${lib.makeBinPath [ nix ]}:$PATH
-          nix flake archive --refresh '${updatesFlakeRef}'
-          nix build --no-link --keep-going --inputs-from '${updatesFlakeRef}' '${flakeRef}#nixosConfigurations.raspberrypi-nixos.config.system.build.toplevel' '${flakeRef}#homeConfigurations.pi.activationPackage' '${flakeRef}#devShells.aarch64-linux.default'
+          nix flake archive --refresh '${flakeRef}'
+          nix build --no-link --keep-going '${flakeRef}#nixosConfigurations.raspberrypi-nixos.config.system.build.toplevel' '${flakeRef}#homeConfigurations.pi.activationPackage' '${flakeRef}#devShells.aarch64-linux.default'
         '';
       startAt = "Fri, 06:00";
     };
