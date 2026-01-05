347d40fc483ef987992d21413911c5f62028f8ea
Adomas Jatužis <adomixaszvers@gmail.com>
2026-01-05 23:47:26 +0200

    add nixos-cuda cache

--- nixos/common.nix
+++ nixos/common.nix
@@ -11,10 +11,12 @@
       keep-outputs = true;
       substituters = lib.mkAfter [
         "https://nix-community.cachix.org"
+        "https://cache.nixos-cuda.org"
         "https://adomixaszvers.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "adomixaszvers.cachix.org-1:r3/lrlbDE7o/Vjk/muEU2iLIiCEZMbC09ZqiwAs64so="
       ];
       auto-optimise-store = true;
--- nixos/pc/configuration.nix
+++ nixos/pc/configuration.nix
@@ -27,15 +27,6 @@
     inputs.sops-nix.nixosModules.sops
   ];
 
-  nix.settings = {
-    substituters = [
-      "https://cache.nixos-cuda.org"
-    ];
-    trusted-public-keys = [
-      "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
-    ];
-  };
-
   boot = {
     initrd.systemd.enable = true;
     loader = {
