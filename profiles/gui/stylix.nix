df97d61ee4cfc22d447fce59b63dbf466169b752
Adomas Jatužis <adomixaszvers@gmail.com>
2026-01-13 23:57:14 +0200

    avoid IFD

--- profiles/gui/stylix.nix
+++ profiles/gui/stylix.nix
@@ -1,6 +1,5 @@
 {
   pkgs,
-  lib,
   inputs,
   ...
 }:
@@ -14,8 +13,28 @@
       sha256 = "03lrj64zig62ibhcss5dshy27kvw363gzygm4rgk7ihbdjj2sw7w";
     };
 
-    polarity = "dark";
-    base16Scheme = lib.mkDefault "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    base16Scheme = {
+      polarity = "dark";
+      scheme = "Catppuccin Mocha";
+      slug = "catppuccin-mocha";
+      author = "https://github.com/catppuccin/catppuccin";
+      base00 = "1e1e2e"; # base
+      base01 = "181825"; # mantle
+      base02 = "313244"; # surface0
+      base03 = "45475a"; # surface1
+      base04 = "585b70"; # surface2
+      base05 = "cdd6f4"; # text
+      base06 = "f5e0dc"; # rosewater
+      base07 = "b4befe"; # lavender
+      base08 = "f38ba8"; # red
+      base09 = "fab387"; # peach
+      base0A = "f9e2af"; # yellow
+      base0B = "a6e3a1"; # green
+      base0C = "94e2d5"; # teal
+      base0D = "89b4fa"; # blue
+      base0E = "cba6f7"; # mauve
+      base0F = "f2cdcd"; # flamingo
+    };
 
     cursor = {
       name = "Banana";
--- profiles/work-remote.nix
+++ profiles/work-remote.nix
@@ -1,11 +1,10 @@
-{ pkgs, ... }:
+{ ... }:
 {
   imports = [
     ./work-common.nix
     ./wm/xsession-common.nix
     ./wm/xmonad
   ];
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/dracula.yaml";
   xsession.numlock.enable = true;
   services = {
     picom.enable = false;
