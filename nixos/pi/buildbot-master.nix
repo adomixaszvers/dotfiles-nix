4e26186afbf4701147f219ed9d7909ea8ea12015
Adomas Jatužis <adomixaszvers@gmail.com>
2026-01-27 20:39:33 +0200

    buildbot: pull from gh more often

--- nixos/pi/buildbot-master.nix
+++ nixos/pi/buildbot-master.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   config,
+  pkgs,
   ...
 }:
 let
@@ -22,6 +23,10 @@ in
   sops.secrets."buildbot-nix/token" = secretConf;
   sops.secrets."buildbot-nix/cachix-signing-key" = secretConf;
   sops.secrets."buildbot-nix/worker-password" = secretConf;
+  sops.secrets."buildbot/ssh-key" = {
+    sopsFile = ./secrets/buildbot.yaml;
+    owner = config.users.users.buildbot.name;
+  };
   sops.templates."buildbot-nix/workers.json".content = ''
     [
       { "name": "darbas", "pass": "${
@@ -76,11 +81,17 @@ in
     # optional expose latest store path as text file
     # outputsPath = "/var/www/buildbot/nix-outputs";
     pullBased = {
-      pollInterval = 3600;
+      pollInterval = 600; # 10 minutes
       repositories = {
         dotfiles-nix-gh = {
-          url = "https://github.com/adomixaszvers/dotfiles-nix.git";
+          url = "git@github.com:adomixaszvers/dotfiles-nix.git";
           defaultBranch = "update_flake_lock_action";
+          sshPrivateKeyFile = config.sops.secrets."buildbot/ssh-key".path;
+          sshKnownHostsFile = pkgs.writeText "github-hosts" ''
+            github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+            github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
+            github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
+          '';
         };
       };
     };
