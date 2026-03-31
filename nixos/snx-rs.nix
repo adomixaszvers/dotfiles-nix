e2d844b25ab0ee2cf8cd658f06dd017939e5a0e6
Adomas Jatužis <adomixaszvers@gmail.com>
2026-03-31 14:06:41 +0300

    snx-rs: make the service more similar to github example
    see https://github.com/ancwrd1/snx-rs/blob/main/docs/nixos.md

--- nixos/snx-rs.nix
+++ nixos/snx-rs.nix
@@ -2,18 +2,23 @@
 {
   environment.systemPackages = [ pkgs.snx-rs ];
   systemd.services.snx-rs = {
-    description = "SNX-RS Service";
+    description = "SNX-RS VPN client for Linux";
     wantedBy = [ "multi-user.target" ];
-    after = [ "network.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
     path = [
       pkgs.iproute2
       pkgs.kmod
-    ];
+      pkgs.networkmanager
+    ]; # for ip, modprobe and nmcli commands
+
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${pkgs.snx-rs}/bin/snx-rs -m command";
-      User = "root";
+      ExecStart = "${pkgs.snx-rs}/bin/snx-rs -m command -l debug";
       Restart = "on-failure";
     };
   };
+
+  # update the firewall rule to allow keepalive traffic
+  networking.firewall.checkReversePath = "loose";
 }
