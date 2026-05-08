77aec04a106fc523bf9ac1ad7c1d2690fda0fbcc
Adomas Jatužis <adomixaszvers@gmail.com>
2026-05-08 15:04:22 +0300

    he: show workflow and etc. files

--- pkgs/he.nix
+++ pkgs/he.nix
@@ -1,7 +1,7 @@
 { writeShellScriptBin, fzf }:
 writeShellScriptBin "he" ''
   cd ~/.config/nixpkgs || exit 1
-  readarray -t files < <(${fzf}/bin/fzf --multi)
+  readarray -t files < <(fd --hidden --exclude=.git| ${fzf}/bin/fzf --multi)
   if [ -n "''${files[0]}" ]; then
     exec $EDITOR "''${files[@]}"
   fi
