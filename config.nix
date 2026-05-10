270fe8465d38b5d16d62ebcf2d1df42f4ddc7983
Adomas Jatužis <adomixaszvers@gmail.com>
2026-05-10 21:23:51 +0300

    cleanup config.nix

--- config.nix
+++ config.nix
@@ -5,18 +5,10 @@
     builtins.elem (lib.getName pkg) [
       "corefonts" # used in onlyoffice
       "idea"
-      "oraclejdk"
-      "sqldeveloper"
       "steam"
-      "steam-original" # steam without fhs
-      "steam-run"
       "steam-unwrapped"
       "unrar"
     ];
   vim.ftNix = false;
   joypixels.acceptLicense = true;
-  permittedInsecurePackages = [
-    "oraclejdk-8u202"
-    "gradle-7.6.6"
-  ];
 }
