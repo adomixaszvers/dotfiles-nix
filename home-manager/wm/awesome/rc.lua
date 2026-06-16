4067da8209a27355bf92372b9ed9f63ad84ad7fe
Adomas Jatužis <adomixaszvers@gmail.com>
2026-06-16 09:03:37 +0300

    awesome: change shortcuts

--- home-manager/wm/awesome/rc.lua
+++ home-manager/wm/awesome/rc.lua
@@ -308,6 +308,9 @@ local globalkeys = gears.table.join(
   awful.key({ modkey }, 'j', function()
     awful.client.focus.byidx(1)
   end, { description = 'focus next by index', group = 'client' }),
+  awful.key({ modkey }, 'c', function()
+    awful.client.focus.byidx(1)
+  end, { description = 'focus next by index', group = 'client' }),
   awful.key({ modkey }, 'k', function()
     awful.client.focus.byidx(-1)
   end, { description = 'focus previous by index', group = 'client' }),
