db7e60235a427d12ec81b9730c82863f1deed333
Adomas Jatužis <adomixaszvers@gmail.com>
2026-09-19 09:54:19 +0300

    renovate: ignore actions/checkout in forgejo

--- flake.nix
+++ flake.nix
@@ -94,6 +94,7 @@
           };
           treefmt = {
             programs = {
+              jsonfmt.enable = true;
               keep-sorted.enable = true;
               nixfmt.enable = true;
               statix = {
